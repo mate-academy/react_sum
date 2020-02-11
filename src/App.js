a20adaa536588981f419e621baffc19de4d03ece
Bohdan Salenik <bohdan.salenik@gmail.com>
2020-02-11 18:10:32 +0200

    My solution

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import './App.css';
+import Sum from './components/Sum';
 
 const App = () => (
-  <h1>React sum</h1>
+  <div className="sum">
+    <h1>SUM OF a and b</h1>
+    <Sum a={2} b={2} />
+    <Sum a={10} b={0} />
+    <Sum a={-5} b={5} />
+  </div>
 );
 
 export default App;
---
+++ src/components/Sum.js
@@ -0,0 +1,5 @@
+import React from 'react';
+// eslint-disable-next-line
+const Sum = ({ a, b }) => (<p>sum of {a} and {b} is {a + b}</p>);
+
+export default Sum;
