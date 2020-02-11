0651e925429ead014f6b0ac522d70e1e3d97f398
aleksandrDnieper <aleksandr.dnieper@gmail.com>
2020-02-11 22:45:36 +0200

    solution2

--- src/components/Sum.js
+++ src/components/Sum.js
@@ -1,28 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-// const Sum = ({ a, b }) => (
-//   <div>{`Sum of ${a} and ${b} is ${a + b}`}</div>
-// );
-
 function Sum(props) {
   const { a, b } = props;
 
-  return (
-    <div>
-Sum of
-      {' '}
-      {a}
-      {' '}
-and
-      {' '}
-      {b}
-      {' '}
-is
-      {' '}
-      {a + b}
-    </div>
-  );
+  return <div>{`Sum of ${a} and ${b} is ${a + b}`}</div>;
 }
 
 Sum.propTypes = {
