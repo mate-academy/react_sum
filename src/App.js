146bdd1fac37c9c8406ab6729ba9686673a5a835
AlexanderShpilka <sandro.bot.evil@gmail.com>
2020-02-11 20:12:18 +0200

    Implement Sum component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import './App.css';
+import Sum from './components/Sum';
+
+const setOfArgs = [[2, 2], [10, 0], [-5, 5]];
 
 const App = () => (
-  <h1>React sum</h1>
+  <div className="App">
+    <h1>Sum Component</h1>
+    <hr />
+
+    {setOfArgs.map((args) => {
+      const [argA, argB] = args;
+
+      return <Sum key={Math.random().toString()} a={argA} b={argB} />;
+    })}
+  </div>
 );
 
 export default App;
