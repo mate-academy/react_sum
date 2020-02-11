768179f416009bea10c6231c648480243d5689ae
Iuliia Tsentylo <labliuk.maksym@gmail.com>
2020-02-11 12:17:37 +0200

    solution

---
+++ build/precache-manifest.37c2abed0caf07731c8502b6dc90031d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05965fa7396fa661f856b95bb0e69923",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c372384f7c5ad731886b",
+    "url": "./static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "b829692f51f58c98b1bd",
+    "url": "./static/js/2.d29b3332.chunk.js"
+  },
+  {
+    "revision": "c372384f7c5ad731886b",
+    "url": "./static/js/main.185b15fa.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
