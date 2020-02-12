000ce5186f824aa9fa6a8ffd515c7bd3e74e4545
oksanaDaniluk <oksana.daniluk@ukr.net>
2020-02-12 16:28:40 +0200

    arrow applied

--- src/components/Sum.js
+++ src/components/Sum.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function Sum({ a, b }) {
-  return (
-    <p>
-      {`Sum of ${a} and ${b} is ${a + b}`}
-    </p>
-  );
-}
+const Sum = ({ a, b }) => (
+  <p>
+    {`Sum of ${a} and ${b} is ${a + b}`}
+  </p>
+);
 
 Sum.propTypes = {
   a: PropTypes.number.isRequired,
