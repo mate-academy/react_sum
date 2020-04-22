e8bb90e389100ae44d532d8a1af294a6b0e6584e
Tetiana <ishchuktr@gmail.com>
2020-04-22 20:15:29 +0300

    solution

--- src/components/Sum.js
+++ src/components/Sum.js
@@ -1,16 +1,9 @@
 import React from 'react';
 
-const Sum = ({ a, b }) => (
-  <div>
-    Sum of
-    {b}
-    {' '}
-    and
-    {a}
-    {' '}
-    is
-    {a + b}
-  </div>
-);
+const Sum = ({a, b}) => {
+  return(
+  <div>Sum of {b} and {a} is {a + b}</div>
+  );
+}
 
 export default Sum;
