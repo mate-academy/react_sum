a1e64fb3088fd7b3bf7af1568742198ced99aae1
MykolaMak <makolamak@gmail.com>
2020-04-22 16:59:05 +0300

    implement Sum component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import './App.css';
+import Sum from './components/Sum';
 
 const App = () => (
-  <h1>React sum</h1>
+  <div>
+    <Sum firstValue={2} secondValue={1} />
+    <Sum firstValue={2} secondValue={2} />
+    <Sum firstValue={10} secondValue={0} />
+    <Sum firstValue={2} secondValue={1} />
+  </div>
 );
 
 export default App;
