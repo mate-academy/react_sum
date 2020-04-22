30afa8f1e54b75afdd73792f364ba57148ffb2ec
Vladimir-Beliy <vovaazick@gmail.com>
2020-04-22 13:45:00 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Sum from './components/Sum';
 
 ReactDOM.render(
   <App />,
+  document.getElementById('root'),
+);
+
+ReactDOM.render(
+  <Sum a={3} b={4} />,
   document.getElementById('root'),
 );
