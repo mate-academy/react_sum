6009dbf50b8d32542580977c594925bb3218445a
olenazubashenko <olenazubashenko@gmail.com>
2020-04-22 22:54:31 +0300

    task

---
+++ src/components/Sum.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Sum = ({a, b}) => {
+
+  return (
+    <p>
+      Sum of {a} and {b} is {a + b}
+    </p>
+  );
+};
+
+Sum.propTypes = {
+  a: PropTypes.number.isRequired,
+  b: PropTypes.number.isRequired,
+
+};
+
+export default Sum;
