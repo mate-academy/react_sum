c21fa60291af1a2b175c305b9ff27ecc036ef5b6
ShapovalDenys <denys.igorevich@gmail.com>
2020-04-22 14:12:54 +0300

    solution v0.1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
 import React from 'react';
 import './App.css';
+import PropTypes from 'prop-types';
+
+const Sum = props => (
+  <p>
+    Sum of&nbsp;
+    {props.a}
+    {' '}
+    and&nbsp;
+    {props.b}
+    {' '}
+    is&nbsp;
+    {props.a + props.b}
+  </p>
+);
 
 const App = () => (
-  <h1>React sum</h1>
+  <div>
+    <Sum a={2} b={2} />
+    <Sum a={10} b={0} />
+    <Sum a={-5} b={5} />
+  </div>
 );
+
+Sum.propTypes = {
+  a: PropTypes.number.isRequired,
+  b: PropTypes.number.isRequired,
+};
 
 export default App;
