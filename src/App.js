056cb208ab3c436144ac1ba895e0b88c969f12c3
Oleksii Mykhailov <alem.coding@gmail.com>
2020-04-23 10:36:10 +0300

    Div changed to fragment

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import './App.css';
 import Sum from './components/Sum';
 
 const App = () => (
-  <div>
+  <>
     <h1>React sum</h1>
     <Sum a={2} b={2} />
     <Sum a={10} b={0} />
     <Sum a={-5} b={5} />
-  </div>
+  </>
 );
 
 export default App;
