f937748d9e7e90bfbaea6def442f7445e125d3d7
Glib Rulev <velzepooz@gmail.com>
2020-06-20 16:54:50 +0300

    Fixed

--- src/components/Sum.js
+++ src/components/Sum.js
@@ -4,17 +4,7 @@ import PropTypes from 'prop-types';
 export default function Sum({ a, b }) {
   return (
     <p>
-      Sum of
-      {' '}
-      {a}
-      {' '}
-      and
-      {' '}
-      {b}
-      {' '}
-      is
-      {' '}
-      {a + b}
+      {`Sum of ${a} and ${b} is ${a + b}`}
     </p>
   );
 }
