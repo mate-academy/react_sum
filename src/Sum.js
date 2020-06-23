674e08dffaf48f07df8d14fb0f00144f0dd5707f
DTR <t.terry.rice@gmail.com>
2020-06-23 11:53:27 +0300

    final 1.0.1

--- src/Sum.js
+++ src/Sum.js
@@ -2,11 +2,7 @@ import PropTypes from 'prop-types';
 import React from 'react';
 
 function Sum({ a, b }) {
-  const result = `
-    Sum of ${a} and ${b} is ${a + b}
-  `;
-
-  return <p>{result}</p>;
+  return <p>{`Sum of ${a} and ${b} is ${a + b}`}</p>;
 }
 
 Sum.propTypes = {
