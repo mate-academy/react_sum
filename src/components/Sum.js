7a724f4037544275e01567fc7081d0718f7112cf
Vladyslav Halushkin <v.halushkin@gmail.com>
2020-06-23 21:24:46 +0300

    task ready

---
+++ src/components/Sum.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Sum = ({ a = 0, b = 0 }) => (
+  <p>
+    Sum of
+    {a}
+    {' '}
+    and
+    {b}
+    {' '}
+    is
+    {a + b}
+  </p>
+);
+
+Sum.propTypes = {
+  a: PropTypes.number.isRequired,
+  b: PropTypes.number.isRequired,
+};
+
+export { Sum };
