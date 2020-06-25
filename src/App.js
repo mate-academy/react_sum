382abfc6a9ff6ef950c387b193526cb60b3ff699
RammsfanHTW <64034634+RammsfanHTW@users.noreply.github.com>
2020-06-25 06:55:50 +0300

    Solution

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import './App.css';
+import propTypes from 'prop-types';
 
-const App = () => (
-  <h1>React sum</h1>
+const App = ({ a, b }) => (
+  <h1>
+    {
+      `Sum of ${a} and ${b} = ${a + b}`
+    }
+  </h1>
 );
+
+App.propTypes = {
+  a: propTypes.number.isRequired,
+  b: propTypes.number.isRequired,
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 ReactDOM.render(
-  <App />,
+  <React.Fragment>
+    <App a={2} b={2} />
+    <App a={10} b={0} />
+    <App a={-5} b={5} />
+  </React.Fragment>,
   document.getElementById('root'),
 );
