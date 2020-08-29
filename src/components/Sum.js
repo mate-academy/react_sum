0c0aab89c9691f7d060f6249d56c98aa0c539728
Dennis Priadka <dennis.priadka@gmail.com>
2020-08-29 21:34:29 +0300

    +

--- src/components/Sum.js
+++ src/components/Sum.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export function Sum(props) {
+export function Sum({ a, b }) {
   return (
     <p>
-      {`Sum of ${props.a} and ${props.b} is ${props.a + props.b}`}
+      {`Sum of ${a} and ${b} is ${a + b}`}
     </p>
   );
 }
