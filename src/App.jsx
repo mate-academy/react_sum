f2f671049acbf72b9d7ed4d78cb11a476a6097bf
Bohdan Leonets <leonetsbogdan@gmail.com>
2020-09-01 15:11:32 +0300

    fixed solution

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,10 @@ import Sum from './components/Sum';
 const App = () => (
   <>
     <h1 className="header">React sum</h1>
-    <p>
-      <Sum a={1} b={2} />
-      <Sum a={2} b={2} />
-      <Sum a={10} b={0} />
-      <Sum a={-5} b={5} />
-    </p>
+    <Sum a={1} b={2} />
+    <Sum a={2} b={2} />
+    <Sum a={10} b={0} />
+    <Sum a={-5} b={5} />
   </>
 );
 
