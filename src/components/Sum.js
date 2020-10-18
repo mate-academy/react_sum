29498247f5881f30c28e823af02c253988d2ce56
Petro <petdavy@1991gmail.com>
2020-10-18 18:38:01 +0300

    fix main

--- src/components/Sum.js
+++ src/components/Sum.js
@@ -2,21 +2,19 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Sum = ({ a, b }) => (
-  <main className="sum">
-    <div className="sum__example">
-      Sum of
-      {' '}
-      <span className="sum__number">{a}</span>
-      {' '}
-      and
-      {' '}
-      <span className="sum__number">{b}</span>
-      {' '}
-      is
-      {' '}
-      <span className="sum__result">{a + b}</span>
-    </div>
-  </main>
+  <div className="sum__example">
+    Sum of
+    {' '}
+    <span className="sum__number">{a}</span>
+    {' '}
+    and
+    {' '}
+    <span className="sum__number">{b}</span>
+    {' '}
+    isnpm
+    {' '}
+    <span className="sum__result">{a + b}</span>
+  </div>
 );
 
 Sum.propTypes = {
