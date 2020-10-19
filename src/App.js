ca55354687e89f90a285603d872de4e6ab65ceaa
Alina <alina90565@gmail.com>
2020-10-19 12:40:19 +0300

    done sum task

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import React from 'react';
+import Sum from './components/Sum';
 import './App.scss';
 
 const App = () => (
-  <h1>React sum</h1>
+  <div className="actile">
+    <h1 className="actile__title">React sum</h1>
+
+    <Sum
+      a={2}
+      b={2}
+    />
+
+    <Sum a={10} />
+
+    <Sum
+      a={-5}
+      b={5}
+    />
+  </div>
 );
 
 export default App;
