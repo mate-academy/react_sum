dec643845d664bd90fbcef052705a144a3b1dab6
Anton <office.sklyarov@gmail.com>
2020-10-20 18:34:46 +0300

    code was refactored

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.scss';
 import Sum from './components/Sum';
 
 const App = () => (
-  <h1 className="title">
-    React sum
+  <div>
+    <h1>
+      React sum
+    </h1>
     <Sum a={2} b={2} />
     <Sum a={10} b={0} />
     <Sum a={-5} b={5} />
-  </h1>
+  </div>
 );
 
 export default App;
