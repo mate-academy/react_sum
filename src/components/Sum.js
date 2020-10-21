707a334ea9c9f6a2ef16cb6509de4efd527dd439
Ivan Beletskiy <beletsky02@gmail.com>
2020-10-21 12:03:15 +0300

    refactoring

--- src/components/Sum.js
+++ src/components/Sum.js
@@ -1,14 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Sum = ({
-  a,
-  b,
-}) => (
+const Sum = ({ a, b }) => (
   <p>
-    {
-      `Sum of ${a} and ${b} is ${a + b}`
-    }
+    {`Sum of ${a} and ${b} is ${a + b}`}
   </p>
 );
 
