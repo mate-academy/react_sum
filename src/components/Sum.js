c9c883c19f957e0dc6a022e18bf2affb2ab97039
Yurii <yurastt1@gmail.com>
2020-10-21 07:33:06 +0300

    sol

---
+++ src/components/Sum.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import propTypes from 'prop-types';
+
+const Sum = ({ a, b }) => (
+  <p>
+    {`sum of ${a} and ${b} is ${a + b}`}
+  </p>
+);
+
+Sum.propTypes = {
+  a: propTypes.number.isRequired,
+  b: propTypes.number.isRequired,
+};
+
+export default Sum;
