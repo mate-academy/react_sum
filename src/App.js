0f166e628dac187e56bf6929f408f9da825f4736
Olexiy Bashmakov <olexiybashmakov@gmail.com>
2020-10-22 12:27:08 +0300

    removed useless tag in App

--- src/App.js
+++ src/App.js
@@ -6,24 +6,9 @@ const App = () => (
   <div>
     <h1>React sum</h1>
 
-    <p>
-
-      <Sum
-        a={2}
-        b={2}
-      />
-
-      <Sum
-        a={10}
-        b={0}
-      />
-
-      <Sum
-        a={-5}
-        b={5}
-      />
-
-    </p>
+    <Sum a={2} b={2} />
+    <Sum a={10} b={0} />
+    <Sum a={-5} b={5} />
   </div>
 );
 
