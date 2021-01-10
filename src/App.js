abe67cce2334f4235ffa9e0b6d01584f9595dce9
Serhii Alieksieievych <kajot1863@gmail.com>
2021-01-10 21:30:01 +0200

    Completed

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './App.scss';
 
-const App = () => (
-  <h1>React sum</h1>
+const App = ({ a, b }) => (
+  <section>
+    <h1>React sum</h1>
+    <div>{+a + +b}</div>
+  </section>
 );
+
+App.propTypes = {
+  a: PropTypes.string.isRequired,
+  b: PropTypes.string.isRequired,
+};
 
 export default App;
