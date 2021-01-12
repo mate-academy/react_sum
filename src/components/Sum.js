7052d8bf1448a411545d091b99afde9a157beedf
tamara ostapets <tamara.ostapec@gmail.com>
2021-01-12 15:27:02 +0200

    Fixed

--- src/components/Sum.js
+++ src/components/Sum.js
@@ -1,25 +1,15 @@
 import React from 'react';
-import propTypes from 'prop-types';
+import PropTypes from 'prop-types';
 
 const Sum = ({ x, y }) => (
   <p>
-    Sum of
-    {' '}
-    {x}
-    {' '}
-    and
-    {' '}
-    {y}
-    {' '}
-    is
-    {' '}
-    {x + y}
+    {`Sum of ${x} and ${y} is ${x + y}`}
   </p>
 );
 
 Sum.propTypes = {
-  x: propTypes.number.isRequired,
-  y: propTypes.number.isRequired,
+  x: PropTypes.number.isRequired,
+  y: PropTypes.number.isRequired,
 };
 
 export default Sum;
