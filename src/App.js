007a597b1bdda44cd263f31f8d447fdc618b02c2
Nazar Valchyshyn <nvalja@gmail.com>
2021-01-12 19:18:54 +0200

    created div as a wrapper

--- src/App.js
+++ src/App.js
@@ -3,25 +3,21 @@ import './App.scss';
 import Sum from './components/Sum';
 
 const App = () => (
-  <h1 className="app">
-    React sum
-
+  <div className="app">
+    <h1>React sum</h1>
     <Sum
       a={2}
       b={2}
     />
-
     <Sum
       a={10}
       b={0}
     />
-
     <Sum
       a={-5}
       b={5}
     />
-
-  </h1>
+  </div>
 );
 
 export default App;
