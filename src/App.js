b8c0ec96175b7f00d3efbfdccf6c5200236f812f
RioDron <riopositivniy@gmail.com>
2021-01-12 13:21:31 +0200

    revisions syntax

--- src/App.js
+++ src/App.js
@@ -3,20 +3,11 @@ import './App.scss';
 import Sum from './components/Sum';
 
 const App = () => (
-  <>
-    <Sum
-      a={2}
-      b={2}
-    />
-    <Sum
-      a={10}
-      b={0}
-    />
-    <Sum
-      a={-5}
-      b={5}
-    />
-  </>
+  <div>
+    <Sum a={2} b={2} />
+    <Sum a={10} b={0} />
+    <Sum a={-5} b={5} />
+  </div>
 );
 
 export default App;
