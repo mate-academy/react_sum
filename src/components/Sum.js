21634f01f5b90ed6c5b48111f36dff5c7fb48cdb
Dima <dimontregyb@email.ua>
2021-01-13 13:35:02 +0200

    refactoring

--- src/components/Sum.js
+++ src/components/Sum.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Sum = ({ a, b }) => {
-  const result = a + b;
-
-  return (
-    <p>
-      {`Sum of ${a} and ${b} is ${result}`}
-    </p>
-  );
-};
+const Sum = ({ a, b }) => (
+  <p>
+    {`Sum of ${a} and ${b} is ${a + b}`}
+  </p>
+);
 
 Sum.propTypes = {
   a: PropTypes.number.isRequired,
