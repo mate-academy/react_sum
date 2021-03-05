f6ecda053293f7a4f59cbf5530a10110477cc448
antoniakstepan <antoniakstepan1@gmail.com>
2021-03-05 15:22:48 +0200

    second commit

--- src/components/Sum.js
+++ src/components/Sum.js
@@ -3,7 +3,9 @@ import PropTypes from 'prop-types';
 
 function Sum({ a, b }) {
   return (
-    <p>{`Sum of ${a} and ${b} is ${a + b}`}</p>
+    <p>
+      {`Sum of ${a} and ${b} is ${a + b}`}
+    </p>
   );
 }
 
