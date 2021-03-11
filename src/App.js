23da8d49bbb007b0c7690790c66c461d398978df
Olexandr Bieliaiev <Shurasava17@gmail.com>
2021-03-11 14:32:45 +0200

    Solution

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import './App.scss';
+import Sum from './components/Sum';
 
 const App = () => (
-  <h1>React sum</h1>
+  <>
+    <h1>React sum</h1>
+    <Sum
+      a={1}
+      b={2}
+    />
+  </>
 );
 
 export default App;
---
+++ src/components/Sum.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Sum = ({ a, b }) => (
+  <p>
+    Sum of
+    {` ${a.toString()} `}
+    and
+    {` ${b.toString()} `}
+    is
+    {` ${(a + b).toString()} `}
+  </p>
+);
+
+Sum.defaultProps = {
+  a: 0,
+  b: 0,
+};
+
+Sum.propTypes = {
+  a: PropTypes.number,
+  b: PropTypes.number,
+};
+
+export default Sum;
