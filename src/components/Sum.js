8fa5d4e3f2eb996485469029190e34a063d975b8
yura0seredyuk <yura.seredyuk0@gmail.com>
2021-03-15 00:37:40 +0200

    Solution v1.1

--- src/components/Sum.js
+++ src/components/Sum.js
@@ -4,7 +4,9 @@ import PropTypes from 'prop-types';
 export default function Sum({ a, b }) {
   return (
     <p>
-      {`Sum of ${a} and ${b} is ${a + b}`}
+      {
+        `Sum of ${a} and ${b} is ${a + b}`
+      }
     </p>
   );
 }
