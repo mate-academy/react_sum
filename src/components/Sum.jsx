0a70c6b88e64d9c11dd2456ebf4db19d65227d49
Nikita Puhovskii <cheliojul@gmail.com>
2021-03-15 21:01:12 +0200

    fix

--- src/components/Sum.jsx
+++ src/components/Sum.jsx
@@ -1,23 +1,27 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function Sum(obj) {
-  const sum = Number(obj.a) + Number(obj.b);
+function Sum({
+  a,
+  b,
+}) {
+  const sum = a + b;
 
   return (
     <p>
-      Sum of
-      {' '}
-      {obj.a}
-      {' '}
-      and
-      {' '}
-      {obj.b}
-      {' '}
-      is
-      {' '}
-      {sum}
+      {`Sum of ${a} and ${b} is ${sum}`}
     </p>
   );
 }
+
+Sum.propTypes = {
+  a: PropTypes.number,
+  b: PropTypes.number,
+};
+
+Sum.defaultProps = {
+  a: 0,
+  b: 0,
+};
 
 export default Sum;
