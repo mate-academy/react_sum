162e90488505efa81b8641927b694fe1bad52532
Artur Shpontak <ashpontak97@gmail.com>
2021-03-16 00:04:59 +0200

    some changes for better reading

--- src/components/Sum.js
+++ src/components/Sum.js
@@ -1,21 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Sum = ({ a, b }) => (
-  <p>
-    Sum of
-    {' '}
-    {a}
-    {' '}
-    and
-    {' '}
-    {b}
-    {' '}
-    is
-    {' '}
-    {a + b}
-  </p>
-);
+const Sum = ({ a, b }) => {
+  const sumString = `Sum of ${a} and ${b} is ${a + b}`;
+
+  return (
+    <p>
+      {sumString}
+    </p>
+  );
+};
 
 Sum.propTypes = {
   a: PropTypes.number.isRequired,
