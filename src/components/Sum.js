c2eb2aadffa819f5eac6305ee31f6586e52c4eb6
Svyat Krit <krit.mgmn@gmail.com>
2021-03-29 17:18:14 +0300

    Fix

--- src/components/Sum.js
+++ src/components/Sum.js
@@ -6,17 +6,7 @@ function Sum({ a, b }) {
 
   return (
     <p>
-      Sum of
-      {' '}
-      {a}
-      {' '}
-      and
-      {' '}
-      {b}
-      {' '}
-      is
-      {' '}
-      {sum}
+      {`Sum of ${a} and ${b} is ${sum}`}
     </p>
   );
 }
