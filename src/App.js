fa244c595c3c87d16aec4ed3d3485eb163f4814b
Makhmut <grupry@gmail.com>
2021-03-29 19:00:15 +0300

    Solution

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import Sum from './components/Sum';
 import './App.scss';
 
-const App = () => (
-  <h1>React sum</h1>
-);
+function App() {
+  const nums = [
+    [2, 2],
+    [10, 0],
+    [-5, 5],
+  ];
+
+  return (
+    <>
+      <h1>React Sum</h1>
+      <Sum nums={nums[0]} />
+      <Sum nums={nums[1]} />
+      <Sum nums={nums[2]} />
+    </>
+  );
+}
 
 export default App;
