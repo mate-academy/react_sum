5748791976275c6c87870ee3e5567bda812b3ab0
Svyat Krit <krit.mgmn@gmail.com>
2021-03-29 17:16:37 +0300

    Solution

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import './App.scss';
+import Sum from './components/Sum';
+
+const input = [[2, 2], [10, 0], [-5, 5]];
 
 const App = () => (
-  <h1>React sum</h1>
+  <React.Fragment>
+    {
+      input.map(item => (
+        <Sum
+          a={item[0]}
+          b={item[1]}
+        />
+      ))
+    }
+  </React.Fragment>
 );
 
 export default App;
