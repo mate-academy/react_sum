d102927cb57abc2dc4b483006ad960b79b69ae5f
Svyat Krit <krit.mgmn@gmail.com>
2021-03-30 11:59:01 +0300

    Fixes

--- src/App.js
+++ src/App.js
@@ -5,16 +5,17 @@ import Sum from './components/Sum';
 const input = [[2, 2], [10, 0], [-5, 5]];
 
 const App = () => (
-  <React.Fragment>
+  <>
     {
       input.map(item => (
         <Sum
+          key={item.toString()}
           a={item[0]}
           b={item[1]}
         />
       ))
     }
-  </React.Fragment>
+  </>
 );
 
 export default App;
