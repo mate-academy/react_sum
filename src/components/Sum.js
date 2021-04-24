e069b7370d80656fa1b2fb7b17e4b25655d9ecd0
Maksym Matuliak <maksymmatuliak@gmail.com>
2021-04-24 15:46:15 +0300

    added spaces

--- src/components/Sum.js
+++ src/components/Sum.js
@@ -4,12 +4,15 @@ import PropTypes from 'prop-types';
 const Sum = ({ a, b }) => (
   <h1>
     Sum of
+    {' '}
     {a}
     {' '}
     and
+    {' '}
     {b}
     {' '}
     is
+    {' '}
     {a + b}
   </h1>
 );
