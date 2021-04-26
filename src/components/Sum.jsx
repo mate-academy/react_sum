44db53736804a65147a72f616983a9cf618f760a
Andriy Ivanyk <andriy@Air-Andriy.lan>
2021-04-27 00:22:34 +0100

    Solution

---
+++ src/components/Sum.jsx
@@ -0,0 +1,16 @@
+
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Sum = ({ num1, num2 }) => (
+  <p>
+      Sum of {num1} and {num2} is {num1 + num1}
+  </p>
+);
+
+Sum.propTypes = {
+  a: PropTypes.number.isRequired,
+  b: PropTypes.number.isRequired,
+};
+
+export default Sum;
