7584b1dc065d555a1055fa6fb6fa0d08414301e0
Serhii Kolesnyk <kolesniks203@gmail.com>
2021-05-17 16:48:34 +0300

    react_sum

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import Sum from './components/Sum';
 import './App.scss';
 
 const App = () => (
-  <h1>React sum</h1>
+  <>
+    <h1>React sum</h1>
+    <Sum a={5} b={-5} />
+  </>
 );
 
 export default App;
