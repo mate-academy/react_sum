886f3ee30d4d84271f34a4c7547c823c49aab8a0
Ivan Grekov <grekovv76@gmail.com>
2021-05-17 18:58:45 +0300

    solution

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 import React from 'react';
+import Sum from './components/Sum';
 import './App.scss';
 
 const App = () => (
-  <h1>React sum</h1>
+  <>
+    <h1>React sum</h1>
+    <Sum
+      a={1}
+      b={2}
+    />
+    <Sum
+      a={10}
+    />
+    <Sum
+      b={10}
+    />
+    <Sum
+      a={-5}
+      b={5}
+    />
+  </>
 );
 
 export default App;
