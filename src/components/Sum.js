db8422ad8f16df9aeec6824346e442263a0d8afb
Bogdan Kotsupey <80522234+Bogdan-Kotsupey@users.noreply.github.com>
2021-05-18 12:51:19 +0300

    fixed

--- src/components/Sum.js
+++ src/components/Sum.js
@@ -3,15 +3,7 @@ import PropTypes from 'prop-types';
 
 const Sum = ({ a = 0, b = 0 }) => (
   <p>
-    Sum of
-    &ensp;
-    {a}
-    and
-    &ensp;
-    {b}
-    is
-    &ensp;
-    {a + b}
+    {`Sum of ${a} and ${b} is ${a + b}`}
   </p>
 );
 
