01c4f6cbc240c5cc7c82e2172d39f54e0103ede8
Dima Batiuk <batiuk20@gmail.com>
2021-05-18 12:01:13 +0300

    done

--- src/App.js
+++ src/App.js
@@ -5,18 +5,9 @@ import SumBlock from './components/SumBlock';
 const App = () => (
   <div className="reactSum">
     <h1 className="pageTitle">React sum</h1>
-    <SumBlock
-      a={2}
-      b={2}
-    />
-    <SumBlock
-      a={10}
-      b={0}
-    />
-    <SumBlock
-      a={-5}
-      b={5}
-    />
+    <SumBlock a={2} b={2} />
+    <SumBlock a={10} b={0} />
+    <SumBlock a={-5} b={5} />
   </div>
 );
 
--- src/components/SumBlock.jsx
+++ src/components/SumBlock.jsx
@@ -1,25 +1,25 @@
 import React from 'react';
 import '../App.scss';
+import PropTypes from 'prop-types';
 
-// eslint-disable-next-line react/prop-types
 const SumBlock = ({ a, b }) => (
   <div className="block">
     <div className="sum">
       <div className="result">
-        Sum of
-        {' '}
-        {a}
-        {' '}
-        and
-        {' '}
-        {b}
-        {' '}
-        is
-        {' '}
-        {a + b}
+        {`Sum of ${a} and ${b} is ${a + b}`}
       </div>
     </div>
   </div>
 );
+
+SumBlock.defaultProps = {
+  a: 0,
+  b: 0,
+};
+
+SumBlock.propTypes = {
+  a: PropTypes.number,
+  b: PropTypes.number,
+};
 
 export default SumBlock;
