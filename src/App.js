cc4ab2bdba7c52075b181548ca5da056794c1c2f
Provolovskyi Illia <provolovskyi.illia@gmail.com>
2021-05-18 13:03:34 +0300

    after_review

--- src/App.js
+++ src/App.js
@@ -6,20 +6,11 @@ import Sum from './components/Sum';
 const App = () => (
   <section className="addNumbers">
     <h1 className="addNumbers__title">React sum</h1>
-    <Sum
-      a={2}
-      b={2}
-    />
+    <Sum a={2} b={2} />
 
-    <Sum
-      a={10}
-      b={0}
-    />
+    <Sum a={10} b={0} />
 
-    <Sum
-      a={-5}
-      b={5}
-    />
+    <Sum a={-5} b={5} />
   </section>
 );
 
