3ac9362364ba46fc62f3d51036a89f9f9e1fc3f3
Ivan Kolodyazhnyi <80517717+IvanKolodiazhnyi@users.noreply.github.com>
2021-05-18 16:05:35 +0300

    rework

--- src/App.js
+++ src/App.js
@@ -5,20 +5,9 @@ import Sum from './components/Sum';
 
 const App = () => (
   <div>
-    <Sum
-      a={2}
-      b={2}
-    />
-
-    <Sum
-      a={10}
-      b={0}
-    />
-
-    <Sum
-      a={-5}
-      b={5}
-    />
+    <Sum a={2} b={2} />
+    <Sum a={10} b={0} />
+    <Sum a={-5} b={5} />
   </div>
 );
 
