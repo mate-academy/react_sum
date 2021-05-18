7c806fcacb2f306162f131f6fc717c8deafd86cd
amithos <alex.theutos@gmail.com>
2021-05-18 12:06:55 +0300

    just a try

--- src/App.js
+++ src/App.js
@@ -1,28 +1,13 @@
 import React from 'react';
 import './App.scss';
 import Sum from './components/Sum';
-import Amount from './components/Amount';
 
 const App = () => (
-
-  // Amount it's a component for
-  // (from readme) => "accepting props `a` and `b`"
-
-  // Sum it's a component for
-  // (from readme) => "for `2 + 2`, `10 + 0` and `-5 + 5`"
-
-  // I will delete a redundant component,
-  // but now I don't which one you need and which one - nope)
-
   <>
     <h1>React sum</h1>
-    <Sum text="2 + 2" />
-    <Sum text="10 + 0" />
-    <Sum text="-5 + 5" />
-
-    <Amount a="2" b="2" />
-    <Amount a="10" b="0" />
-    <Amount a="-5" b="5" />
+    <Sum a={2} b={2} />
+    <Sum a={10} b={0} />
+    <Sum a={-5} b={5} />
   </>
 );
 
--- src/components/Sum.js
+++ src/components/Sum.js
@@ -1,25 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Sum = (props) => {
-  const data = props.text.split(' + ');
-
-  return (
-    <p>
-      Sum of
-      {data[0]}
-      {' '}
-      and
-      {data[1]}
-      {' '}
-      is
-      {+data[0] + +data[1]}
-    </p>
-  );
-};
+const Sum = ({ a, b }) => (
+  <p>
+    Sum of
+    {' '}
+    {a}
+    {' '}
+    and
+    {' '}
+    {b}
+    {' '}
+    is
+    {' '}
+    {a + b}
+  </p>
+);
 
 Sum.propTypes = {
-  text: PropTypes.string.isRequired,
+  a: PropTypes.number.isRequired,
+  b: PropTypes.number.isRequired,
 };
 
 export default Sum;
