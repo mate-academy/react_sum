5bfd371bf6c75078454ce8a6cc42300bf240dbd5
Anton Kostylov <antoniok003@gmail.com>
2021-05-18 12:15:35 +0300

    Fixes according to reviev

--- src/App.js
+++ src/App.js
@@ -1,29 +1,13 @@
 import React from 'react';
 import './App.scss';
-import propTypes from 'prop-types';
+import Sum from './components/Sum';
 
-const App = ({
-  a,
-  b,
-}) => (
-  <h1>
-    Sum of
-    {' '}
-    {a}
-    {' '}
-    and
-    {' '}
-    {b}
-    {' '}
-    is
-    {' '}
-    {a + b}
-  </h1>
+const App = () => (
+  <>
+    <Sum a={2} b={2} />
+    <Sum a={10} b={0} />
+    <Sum a={-5} b={5} />
+  </>
 );
-
-App.propTypes = {
-  a: propTypes.number.isRequired,
-  b: propTypes.number.isRequired,
-};
 
 export default App;
