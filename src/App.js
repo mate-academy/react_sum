bd54536f1b94a54fc5722294601b9955c882a8bd
Igor Stasiv <igorstsv@gmail.com>
2021-05-18 14:15:07 +0300

    Fixed linter problem

--- src/App.js
+++ src/App.js
@@ -5,20 +5,10 @@ import Sum from './components/Sum';
 
 const App = () => (
   <>
-    <Sum
-      a={2}
-      b={2}
-    />
-
-    <Sum
-      a={10}
-      b={0}
-    />
-
-    <Sum
-      a={-5}
-      b={5}
-    />
+    <h1>React sum</h1>
+    <Sum a={2} b={2} />
+    <Sum a={10} b={0} />
+    <Sum a={-5} b={5} />
   </>
 );
 
