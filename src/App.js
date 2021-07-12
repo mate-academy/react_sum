ee45598533fb043abb9c930a7f5c7b8311038c8f
Artem Yemelyanov <ae1666528@gmail.com>
2021-07-12 17:07:08 +0300

    Fixed

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.scss';
 const App = () => (
   <div>
     <Sum left={2} right={2} />
-    <Sum left={10} right={0} />
+    <Sum left={10} />
     <Sum left={-5} right={5} />
   </div>
 );
