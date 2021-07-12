fde5a84ae77ba7d252229b3c82d3de35b338c5c4
VolodymyrKlepykov <volodymyr.klepykov@gmail.com>
2021-07-12 14:19:34 +0300

    fix: change teg div to p

--- src/components/Sum.js
+++ src/components/Sum.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Sum = ({ a, b }) => (
-  <p>
-    {`Sum of ${a} and ${b} is ${a + b}.`}
-  </p>
-);
-
-Sum.defaultProps = {
-  a: 0,
-  b: 0,
-};
+function Sum({
+  a = 0,
+  b = 0,
+}) {
+  return (
+    <p>
+      {`Sum of ${a} and ${b} is ${a + b}`}
+    </p>
+  );
+}
 
 Sum.propTypes = {
-  a: PropTypes.number,
-  b: PropTypes.number,
+  a: PropTypes.number.isRequired,
+  b: PropTypes.number.isRequired,
 };
 
 export default Sum;
