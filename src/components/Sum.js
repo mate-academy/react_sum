82e6df2b60dd0032dea831a0621d6e4ab2a41b53
David Dohru <tronsmartmy@gmail.com>
2021-07-13 23:10:54 +0300

    done

---
+++ src/components/Sum.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Sum({
+  a = 0,
+  b = 0,
+}) {
+  return (
+    <p>
+      Sum of
+      {' '}
+      {a}
+      {' '}
+      and
+      {' '}
+      {b}
+      {' '}
+      is
+      {' '}
+      {a + b}
+    </p>
+  );
+}
+
+Sum.propTypes = {
+  a: PropTypes.number.isRequired,
+  b: PropTypes.number.isRequired,
+};
+
+export default Sum;
