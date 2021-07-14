ed223068421236ccd431934acc52cbbe44f1cdba
Vitaliy Martynyuk <vitaliymartiniuk16@gmail.com>
2021-07-14 18:12:58 +0300

    sum

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import './App.scss';
+import Sum from './components/Sum';
 
 const App = () => (
-  <h1>React sum</h1>
+  <>
+    <h1>React sum</h1>
+    <Sum
+      firstValue={2}
+      secondValue={2}
+    />
+    <Sum
+      firstValue={10}
+      secondValue={0}
+    />
+    <Sum
+      firstValue={-5}
+      secondValue={5}
+    />
+  </>
 );
 
 export default App;
