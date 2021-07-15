1b5381b510ba961ccc0c1c2398596b01d5db714d
Maksym Starodubtsev <maksym.starodubtsev@gmail.com>
2021-07-15 14:53:54 +0300

    fix default

--- src/components/Sum/Sum.jsx
+++ src/components/Sum/Sum.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function Sum({ a, b }) {
+function Sum({ a = 0, b = 0 }) {
   return (
     <p>
       {`Sum of ${a} and ${b} is ${a + b}`}
