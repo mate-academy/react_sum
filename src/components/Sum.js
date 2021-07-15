ec29135d12e18308da47f406a4f9858128351345
NYaremchuk <nazarmails@gmail.com>
2021-07-15 20:42:03 +0300

    Solution

---
+++ src/components/Sum.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Sum({ a, b }) {
+  return (
+    <p>
+      Sum of
+      {' '}
+      {a}
+      {' '}
+      and
+      {b}
+      {' '}
+      is
+      {a + b}
+    </p>
+  );
+}
+
+Sum.defaultProps = {
+  a: 2,
+  b: 1,
+};
+
+Sum.propTypes = {
+  a: PropTypes.number,
+  b: PropTypes.number,
+};
+
+export default Sum;
