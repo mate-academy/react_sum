511cdd21461e77a1f1fcb5b071f238022e68f23a
Anastasiia Diachenko <anastasiiadiachenko1@gmail.com>
2021-07-15 16:21:04 +0300

    changed div to p

--- src/components/Sum.js
+++ src/components/Sum.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Sum = ({ a, b }) => (
-  <div>{`Sum of ${a} and ${b} is ${a + b}`}</div>
+  <p>{`Sum of ${a} and ${b} is ${a + b}`}</p>
 );
 
 Sum.defaultProps = {
