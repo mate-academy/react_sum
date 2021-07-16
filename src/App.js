4d453df2010d984ea24d29485ada73be02e9b653
Grygoriy Ponomarenko <profit4fam@gmail.com>
2021-07-16 14:59:47 +0300

    after mentor review

--- src/App.js
+++ src/App.js
@@ -6,9 +6,15 @@ const App = () => (
   <>
     <h1 className="title">React sum</h1>
     <article className="sum">
-      <p className="sum--1"><Sum a={2} b={2} /></p>
-      <p className="sum--2"><Sum a={10} b={0} /></p>
-      <p className="sum--3"><Sum a={-5} b={5} /></p>
+      <p className="sum--1">
+        <Sum a={2} b={2} />
+      </p>
+      <p className="sum--2">
+        <Sum a={10} b={0} />
+      </p>
+      <p className="sum--3">
+        <Sum a={-5} b={5} />
+      </p>
     </article>
   </>
 );
--- src/components/Sum.jsx
+++ src/components/Sum.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function Sum(props) {
-  const { a, b } = props;
+function Sum({ a, b }) {
   const s = a + b;
 
   return (
