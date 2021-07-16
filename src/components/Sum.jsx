f9ec5911076416d3b378a81a4d23b5fb80512d31
Oleh Pidverbetskyi <PidverbetskiyOleg1@gmail.com>
2021-07-16 14:59:11 +0300

    rework

--- src/components/Sum.jsx
+++ src/components/Sum.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Sum = ({ a = 0, b = 0 }) => (
+const Sum = ({ a, b }) => (
   <p className="Sum">
     {`Sum of ${a} and ${b} is ${a + b}.`}
   </p>
