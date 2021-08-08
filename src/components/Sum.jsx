d926acb36d8b75d922a0af71187411bf5ff83b4f
Yulia Shabelna <shabelna28@ukr.net>
2021-08-08 11:49:21 +0300

    NewSolution

--- src/components/Sum.jsx
+++ src/components/Sum.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Sum = (a = 0, b = 0) (
     <p>
@@ -12,3 +13,8 @@ const Sum = (a = 0, b = 0) (
 );
 
 export default Sum;
+
+Sum.PropTypes = {
+    a: PropTypes.number,
+    b: PropTypes.number,
+};
