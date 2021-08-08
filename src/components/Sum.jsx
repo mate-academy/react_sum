e7ca1aa90b34ac469654ec2af1465b158f01cd75
Yulia Shabelna <shabelna28@ukr.net>
2021-08-08 11:00:42 +0300

    solution

--- src/components/Sum.jsx
+++ src/components/Sum.jsx
@@ -1,1 +1,15 @@
-// Don't forget to add default values an check types of props
+import React from 'react';
+
+const Sum = (a = 0, b = 0) (
+    <p>
+        {'Sum of'}
+        {a} 
+        {'and'}
+        {b} 
+        {'is'}
+        {a + b}
+    </p>
+);
+
+
+export default Sum;
