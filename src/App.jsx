7b164e2d3c1edd70ed5a2471d9878652b781ee38
Yulia2806 <shabelna28@ukr.net>
2021-08-10 20:54:34 +0300

    SolutionWithNewComponents

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,13 @@ import './App.scss';
 import Sum from 'Sum';
 
 const App = () => (
-  <Sum />
+  <>
+    <Sum a ={2} b ={3} />
+    <Sum a ={-5} b ={5} />
+    <Sum a ={10} b ={0} />
+    <Sum a ={0} b ={5} />
+    <Sum a ={0} b ={0} />
+  </>  
 );
 
 export default App;
--- src/components/Sum.jsx
+++ src/components/Sum.jsx
@@ -2,19 +2,19 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Sum = (a = 0, b = 0) (
-    <p>
-        {'Sum of'}
-        {a} 
-        {'and'}
-        {b} 
-        {'is'}
-        {a + b}
-    </p>
+  <p>
+    {'Sum of'}
+    {a} 
+    {'and'}
+    {b} 
+    {'is'}
+    {a + b}
+  </p>
 );
 
 export default Sum;
 
 Sum.PropTypes = {
-    a: PropTypes.number,
-    b: PropTypes.number,
+  a: PropTypes.number,
+  b: PropTypes.number,
 };
