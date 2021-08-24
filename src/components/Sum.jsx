152de7fcb042212f43a819b2b65cc1af922b10df
SashaRudenko <for_london@ukr.net>
2021-08-24 16:51:08 +0300

    solution PropTypes added

--- src/components/Sum.jsx
+++ src/components/Sum.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types'
 
 function Sum( { a , b} ) {
   return (
@@ -16,6 +17,11 @@ function Sum( { a , b} ) {
 Sum.defaultProps = {
   a: 0,
   b: 0
+};
+
+Sum.propTypes = {
+  a: PropTypes.number,
+  b: PropTypes.number
 };
 
 export default Sum;
