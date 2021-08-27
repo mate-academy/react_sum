2b97346b23066bbeebdb6f2308159c47b673cac8
Oleksandr Morozov <oleksandr.morozov@protonmail.com>
2021-08-27 16:52:42 +0300

    changes have been implemented

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,27 @@ import './App.scss';
 import { Sum } from './components/Sum';
 
 const App = () => (
+<>
+  <Sum
+    a={2}
+    b={3}
+  />
+
   <Sum
     a={5}
-    b={6}
+    b={-5}
   />
+
+  <Sum
+    a={10}
+  />
+
+  <Sum
+    b={5}
+  />
+
+  <Sum />
+</>
 );
 
 export default App;
