c21199b81cb39f1dbebefdf5c5f0172e1e0f0b94
Mykhailo Ostapysko <mp4clips@gmail.com>
2021-08-30 22:17:14 +0300

    version 1.0

--- src/components/Sum.jsx
+++ src/components/Sum.jsx
@@ -1,1 +1,31 @@
-// Don't forget to add default values an check types of props
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Sum = ({
+  a,
+  b,
+  c,
+}) => (
+  <p>
+    {`Sum of `}
+    {a}
+    {` and `}
+    {b}
+    {` is `}
+    {c}
+  </p>
+);
+
+Sum.defaultProps = {
+  a: 0,
+  b: 0,
+  c: 0,
+};
+
+Sum.propTypes = {
+  a: PropTypes.number,
+  b: PropTypes.number,
+  c: PropTypes.number,
+};
+
+export default Sum;
