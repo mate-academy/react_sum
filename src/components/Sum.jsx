42497db03bc24e01ec749b9c9e648c33ca5cb883
Oleksandr Morozov <oleksandr.morozov@protonmail.com>
2021-08-30 11:39:03 +0300

    object destructuring has been changed

--- src/components/Sum.jsx
+++ src/components/Sum.jsx
@@ -1,19 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export const Sum = ({
-  a,
-  b,
-}) => (
-  <p>
-    {'Sum of '}
-    {a}
-    {' and '}
-    {b}
-    {' is '}
-    {a + b}
-  </p>
-);
+export const Sum = (props) => {
+  const { a, b } = props;
+
+  return (
+    <p>
+      {'Sum of '}
+      {a}
+      {' and '}
+      {b}
+      {' is '}
+      {a + b}
+    </p>
+  );
+};
 
 Sum.defaultProps = {
   a: 0,
