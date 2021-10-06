959e20d3596ecb672ae22d411cd80bdecc801551
magithub@yandex.ru <magithub@yandex.ru>
2021-10-06 17:26:40 +0300

    solution

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,9 @@
 module.exports = {
-  // extends: ['@mate-academy/eslint-config-react-typescript', 'plugin:cypress/recommended'],
-  extends: ['@mate-academy/eslint-config-react', 'plugin:cypress/recommended'],
+  extends: [
+    '@mate-academy/eslint-config-react',
+    'plugin:cypress/recommended',
+  ],
+  rules: {
+    'linebreak-style': 'off',
+  },
 };
