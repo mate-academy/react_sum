fb9ce0ce2024534eb31c769fb1ef481ecb149aed
Igor <i.kornienko1199@gmail.com>
2021-10-07 18:32:33 +0300

    correct varriables

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,9 @@ import { Sum } from './components/Sum';
 const App = () => (
   <>
     <Sum a={2} b={3} />
-    <Sum a={5} b={-5} />
-    <Sum a={10} b={-10} />
-    <Sum a={0} b={5} />
+    <Sum a={-5} b={5} />
+    <Sum a={10} />
+    <Sum b={5} />
     <Sum />
   </>
 );
