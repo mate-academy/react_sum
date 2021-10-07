98b390831fc941124f27ed8e4d598b9222263d57
Iurii Medvedchuk <io.medvedchuk@gmail.com>
2021-10-07 18:33:13 +0300

    solution

--- src/components/Sum.jsx
+++ src/components/Sum.jsx
@@ -1,1 +1,15 @@
-// Don't forget to add default values an check types of props
+import PropTypes from 'prop-types';
+
+export const Sum = ({ a, b }) => (
+  `Sum of ${a} and ${b} is ${a + b}`
+);
+
+Sum.defaultProps = {
+  a: 0,
+  b: 0,
+};
+
+Sum.PropTypes = {
+  a: PropTypes.number,
+  b: PropTypes.number,
+};
