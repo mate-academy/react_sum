81544f70a8e42905d64336a82aac6ec0027070ef
Oleksij Lyashenko <oleksijlyashenko@MacBook-Air-Mary.local>
2021-10-28 17:09:33 +0300

    solution

--- src/components/Sum.jsx
+++ src/components/Sum.jsx
@@ -1,18 +1,31 @@
-/* eslint-disable react/jsx-one-expression-per-line */
-/* eslint-disable no-trailing-spaces */
-/* eslint-disable react/prop-types */
 import React from 'react';
+import PropTypes from 'prop-types';
 
 export const Sum = ({
-  n1 = 0,
-  n2 = 0,
+  n1,
+  n2,
 }) => (
   <p>
-    Sum of{' '}
-    {n1}{' '}
-    and{' '}
-    {n2}{' '}
-    is{' '}
+    Sum of
+    {' '}
+    {n1}
+    {' '}
+    and
+    {' '}
+    {n2}
+    {' '}
+    is
+    {' '}
     {n1 + n2}
   </p>
 );
+
+Sum.defaultProps = {
+  n1: 0,
+  n2: 0,
+};
+
+Sum.propTypes = {
+  n1: PropTypes.number,
+  n2: PropTypes.number,
+};
