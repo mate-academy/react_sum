e5635aa02885f49dd6b242938cc2128c4c488d81
Misha Rosa <misharosa.lviv@gmail.com>
2021-11-03 16:02:35 +0200

    types and default was addad

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,15 @@
 import React from 'react';
 import './App.scss';
+import Sum from './components/Sum';
 
 const App = () => (
   <>
-    <p><Sum a={2} b={3} /></p>
-    <p><Sum a={-5} b={5} /></p>
-    <p><Sum a={10} b={0} /></p>
-    <p><Sum a={0} b={5} /></p>
-    <p><Sum a={0} b={0} /></p>
+    <Sum a={2} b={3} />
+    <Sum a={-5} b={5} />
+    <Sum a={10} b={0} />
+    <Sum a={0} b={5} />
+    <Sum a={0} b={0} />
   </>
-);
-
-const Sum = props => (
-  `Sum of ${props.a} and ${props.b} is ${props.a + props.b}`
 );
 
 export default App;
