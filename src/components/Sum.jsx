a5d67c873cfbd51880a633a749747d32025643df
cyb1ove <cyb1ove.ua@gmail.com>
2021-11-03 15:59:39 +0300

    Some fixes

--- src/components/Sum.jsx
+++ src/components/Sum.jsx
@@ -5,14 +5,9 @@ const Sum = ({ a, b }) => (
   <p>{`Sum of ${a} and ${b} is ${a + b}`}</p>
 );
 
-Sum.defaultProps = {
-  a: 0,
-  b: 0,
-};
-
 Sum.propTypes = {
-  a: PropTypes.number,
-  b: PropTypes.number,
+  a: PropTypes.number.isRequired,
+  b: PropTypes.number.isRequired,
 };
 
 export default Sum;
