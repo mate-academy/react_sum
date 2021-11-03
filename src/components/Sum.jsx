1fbfdc125bdf5c9d396fa5bc9dad8a456503dc05
HaqpaHR <pereverzev.viacheslav@gmail.com>
2021-11-03 19:18:55 +0200

    change string

--- src/components/Sum.jsx
+++ src/components/Sum.jsx
@@ -6,13 +6,7 @@ function Sum({ a = 0, b = 0 }) {
 
   return (
     <p>
-      {`Sum of
-      ${a}
-      and
-      ${b}
-      is
-      ${sum}
-    `}
+      {`Sum of ${a} and ${b} is ${sum}`}
     </p>
   );
 }
