b4d86da3c48570ae48cf6dba14907f54a5e87cf7
Olexandr Yatsykovych <sashayatsikovic@gmail.com>
2021-11-04 09:33:51 +0200

    fixes after code review

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,17 @@
 import React from 'react';
-// eslint-disable-next-line
-import { Sum } from './components/Sum/';
+import { Sum } from './components/Sum';
 
 import './App.scss';
 
 export const App = () => (
   <>
-    <Sum
-      a={2}
-      b={3}
-    />
+    <Sum a={2} b={3} />
 
-    <Sum
-      a={-5}
-      b={5}
-    />
-    <Sum
-      a={10}
-    />
+    <Sum a={-5} b={5} />
 
-    <Sum
-      b={5}
-    />
+    <Sum a={10} />
+
+    <Sum b={5} />
 
     <Sum />
   </>
