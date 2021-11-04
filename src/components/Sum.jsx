576a741d736de1ea6af1e91090fc08c03b13772c
Volodymyr Taranin <jaffplay@gmail.com>
2021-11-04 11:29:27 +0200

    replace default paramateres to default props

--- src/components/Sum.jsx
+++ src/components/Sum.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export const Sum = ({ a = 0, b = 0 }) => (
+export const Sum = ({ a, b }) => (
   <p>
     {`Sum of ${a} and ${b} is ${a + b}`}
   </p>
 );
+
+Sum.deafaultProps = {
+  a: 0,
+  b: 0,
+};
 
 Sum.propTypes = {
   a: PropTypes.number.isRequired,
