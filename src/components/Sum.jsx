cc4e22d62c45d332efc2d828a81f0989b051a9b3
Svyatoslav Victor <victorsvyatoslav@gmail.com>
2021-11-12 11:23:02 +0200

    Task Corrected

--- src/components/Sum.jsx
+++ src/components/Sum.jsx
@@ -3,18 +3,15 @@ import PropTypes from 'prop-types';
 
 const Sum = ({ a, b }) => (
   <p>
-    Sum
-    of
-    {' '}
-    {a}
-    {' '}
-    and
-    {' '}
-    {b}
-    {' '}
-    is
-    {' '}
-    {a + b}
+    {`
+      Sum
+      of
+      ${a}
+      and
+      ${b}
+      is
+      ${a + b}
+    `}
   </p>
 );
 
