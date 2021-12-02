99fb916ada061a5d4cc5ceb31bcb2b3a57f5a0ca
Yevhen Khmelnytskyi <summerssscrystal@gmail.com>
2021-12-02 20:57:12 +0200

    solution was added

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import './App.scss';
+import { Sum } from './components/Sum';
 
 const App = () => (
-  <>
-    <p>Sum of 2 and 3 is 5</p>
-    <p>Sum of -5 and 5 is 0</p>
-    <p>Sum of 10 and 0 is 10</p>
-    <p>Sum of 0 and 5 is 5</p>
-    <p>Sum of 0 and 0 is 0</p>
-    {/* Replace paragraphs with Sum componets */}
-    {/* And remove comment lines :) */}
-  </>
+  <div className="container">
+    <div className="container__sums">
+      <Sum num1={2} num2={3} />
+      <Sum num1={-5} num2={5} />
+      <Sum num1={10} />
+      <Sum num2={5} />
+      <Sum />
+    </div>
+  </div>
 );
 
 export default App;
