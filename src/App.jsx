58e1bf6ddac2d60b68d7ac71fe7cb7d828f03573
Oleksandr Nesterenko <stan0men@yahoo.com>
2021-12-09 15:43:34 +0200

    fix extra lines, add Sum with no params

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,11 @@ import Sum from './components/Sum';
 
 const App = () => (
   <>
-    <Sum
-      a={3}
-      b={3}
-    />
-    <Sum
-      a={5}
-      b={-5}
-    />
-    <Sum
-      a={10}
-    />
-    <Sum
-      b={5}
-    />
+    <Sum a={3} b={3} />
+    <Sum a={5} b={-5} />
+    <Sum a={10} />
+    <Sum b={5} />
+    <Sum />
   </>
 );
 
