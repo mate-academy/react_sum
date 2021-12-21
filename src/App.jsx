4995b58201844e080509b271e4889edc83961be2
Dima Permiakov <dimapermyakov2532@gmail.com>
2021-12-21 15:53:22 +0200

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,10 @@ import { Sum } from './components/Sum';
 import './App.scss';
 
 const App = () => (
-  <>
-    <Sum
-      a={2}
-      b={3}
-    />
-    <Sum
-      a={-5}
-      b={5}
-    />
-    <Sum a={10} />
-    <Sum b={5} />
-    <Sum />
-  </>
+  <Sum
+    a={2}
+    b={1}
+  />
 );
 
 export default App;
