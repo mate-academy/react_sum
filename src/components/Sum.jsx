5aac38903116af64184c63b5cde503bad46abab3
Oleksandr Strilchuk <virass.m@gmail.com>
2021-12-21 20:25:49 +0200

    Complete
    [DEMO LINK](https://Virass.github.io/react_sum/)

---
+++ src/components/Sum.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Sum = ({ a, b }) => (
+  <p>
+    Sum of
+    {` ${a} `}
+    and
+    {` ${b} `}
+    is
+    {` ${a + b} `}
+  </p>
+);
+
+Sum.defaultProps = {
+  a: 0,
+  b: 0,
+};
+
+PropTypes.objectOf(PropTypes.number);
+
+export default Sum;
