aa0605a3428a4f02fb4e492089d7c6acdd2a1593
Alex Ivanov <bcmp4p1@gmail.com>
2022-01-13 15:02:13 +0200

    save

---
+++ src/components/Sum.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export const Components = (
+  a,
+  b,
+) => (
+  <p>
+    {`Sum of ${a} and ${b} is ${a + b}`}
+  </p>
+);
+
+Components.defaultProps = {
+  a: 0,
+  b: 0,
+};
+
+Components.propTypes = {
+  a: PropTypes.number,
+  b: PropTypes.number,
+};
