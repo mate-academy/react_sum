d577c066dce8ee038280390e3b8ef126f766127e
Volodymyr Oleksiienko <oleksienkovladimir50@gmail.com>
2022-01-13 20:42:39 +0200

    done

---
+++ src/components/Sum/Sum.jsx
@@ -0,0 +1,15 @@
+import PropTypes from 'prop-types';
+
+export const Sum = ({ a, b }) => (
+  `Sum of ${a} and ${b} is ${a + b}`
+);
+
+Sum.defaultProps = {
+  a: 0,
+  b: 0,
+};
+
+Sum.propTypes = {
+  a: PropTypes.number.isRequired,
+  b: PropTypes.number,
+};
