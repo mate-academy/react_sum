c857b3937c1bb5b60995d43daaeaca2d01366c2b
izzilowe <izzilowe7@gmail.com>
2022-01-13 15:08:44 +0200

    delite scss

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import './App.scss';
 import Sum from './components/Sum';
 
 const App = () => (
@@ -7,7 +6,7 @@ const App = () => (
     <Sum a={2} b={3} />
     <Sum a={5} b={-5} />
     <Sum a={10} />
-    <Sum b={5} />
+    <Sum />
     <Sum />
   </>
 );
