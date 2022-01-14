e155c4c9b7e3e08dd5c18f0e3871c62a950bd292
Mikedacheka <mikedacheka@gmail.com>
2022-01-14 11:32:52 +0200

    Comment lines deleted

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,9 @@ const App = () => (
   <>
     <Sum a={2} b={3} />
     <Sum a={-5} b={5} />
-    <Sum a={10} b={0} />
     <Sum a={10} />
-    <p>Sum of 10 and 0 is 10</p>
-    <p>Sum of 0 and 5 is 5</p>
-    <p>Sum of 0 and 0 is 0</p>
-    {/* Replace paragraphs with Sum componets */}
-    {/* And remove comment lines :) */}
+    <Sum b={5} />
+    <Sum />
   </>
 );
 
