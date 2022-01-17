ee21364ea75bfe005cd815e53ab6e4420494dd7c
Viktor Bachynskyi <viktorbachinskiy@gmail.com>
2022-01-17 14:24:30 +0200

    upd

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,11 @@ import { Sum } from './components/Sum';
 
 const App = () => (
   <>
-    <p><Sum a={2} b={3} /></p>
-    <p><Sum a={-5} b={5} /></p>
-    <p><Sum a={10} /></p>
-    <p><Sum b={5} /></p>
-    <p><Sum /></p>
+    <p><Sum a={2} b={3} />
+    <p><Sum a={-5} b={5} />
+    <p><Sum a={10} />
+    <p><Sum b={5} />
+    <p><Sum />
   </>
 );
 
