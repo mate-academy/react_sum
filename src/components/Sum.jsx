f624cc9ffa6e936e7385b03a42263f91ce75baa1
Andriy Fesych <andriyhimself@gmail.com>
2022-02-09 00:52:05 +0200

    Solution_0.2__+extra

--- src/components/Sum.jsx
+++ src/components/Sum.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import './Sum.scss';
+import PropTypes from 'prop-types';
 
-const Sum = ({ a = 0, b = 0 }) => (
+// eslint-disable-next-line arrow-parens
+const Sum = (props) => (
   <p>
-    {`Sum of ${a} and ${b} is ${a + b}`}
+    {`Sum of ${props.a} and ${props.b} is ${props.a + props.b}`}
   </p>
 );
+
+Sum.propTypes = {
+  a: PropTypes.number,
+  b: PropTypes.number,
+};
+
+Sum.defaultProps = {
+  a: 0,
+  b: 0,
+};
 
 export default Sum;
