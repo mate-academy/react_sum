e3fc453db34f9865fc551d22e68038e10ec6fa28
Олег Майборода <xa12850446@student.karazin.ua>
2022-02-11 11:04:02 +0200

    solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,38 @@
 import React from 'react';
 import './App.scss';
 
-const Sum =({a = 0, b = 0})=> (
+const Sum = ({ a = 0, b = 0 }) => (
   <p>{`Sum of ${a} and ${b} is ${a + b}`}</p>
-)
+);
+
 const App = () => (
   <>
-  <Sum
-
-    a={2}
-    b={3}
-
-  />
-
-  <Sum
-
-    a = {-5}
-    b = {5}
-
-  />
-
-  <Sum
-
-    a = {10}
-    b = {0}
-
-  />
     <Sum
-
-     a = {0}
-     b = {5}
+      a={2}
+      b={3}
 
     />
+
     <Sum
+      a={-5}
+      b={5}
 
-      a = {0}
-      b = {0}
+    />
 
+    <Sum
+      a={10}
+      b={0}
+
+    />
+
+    <Sum
+      a={0}
+      b={5}
+    />
+
+    <Sum
+      a={0}
+      b={0}
     />
   </>
 );
