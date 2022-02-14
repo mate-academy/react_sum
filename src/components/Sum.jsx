737c750609114acf22fc848ba871809067737eb2
Heorhii Plugoyi <gladiator943work@gmail.com>
2022-02-14 13:02:09 +0200

    save

--- src/components/Sum.jsx
+++ src/components/Sum.jsx
@@ -1,4 +1,7 @@
+import React from 'react';
+
 export const Sum = ({ a = 0, b = 0 }) => (
-  // eslint-disable-next-line react/react-in-jsx-scope
-  <p>{`Sum of ${a} and ${b} is ${a + b}`}</p>
+  <p>
+    {`Sum of ${a} and ${b} is ${a + b}`}
+  </p>
 );
