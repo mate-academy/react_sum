ed4876fd8168720cefd33ead49a86516bc976499
Roma Boreiko <roma.boreiko@gmail.com>
2022-03-24 11:25:22 +0200

    Changes after code rewiev

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
+import { Sum } from './components/Sum';
 import './App.scss';
-import Sum from './components/Sum';
 
 const App = () => (
   <>
     <Sum a={2} b={3} />
-    <Sum a={-5} b={5} />
+    <Sum a={5} b={-5} />
     <Sum a={10} />
     <Sum b={5} />
     <Sum />
--- src/components/Sum.jsx
+++ src/components/Sum.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const Sum = ({ a = 0, b = 0 }) => (
+export const Sum = ({ a, b }) => (
   <p>{`Sum of ${a} and ${b} is ${a + b}`}</p>
 );
 
-export default Sum;
+Sum.defaultProps = {
+  a: 0,
+  b: 0,
+};
+
+Sum.propTypes = {
+  a: PropTypes.number,
+  b: PropTypes.number,
+};
