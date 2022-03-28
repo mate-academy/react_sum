22ef479d6b8601c860bb0776e661e17c9433b220
y.kostenko <y.kostenko@mate.academy>
2022-03-28 17:32:43 +0300

    Revert "cypress tests added"
    This reverts commit 62d8ea725fabe9f1ad8bd64bb1cedef8a214c640.

--- cypress/integration/page.spec.js
+++ cypress/integration/page.spec.js
@@ -1,19 +1,7 @@
-import Sum from '../../src/components/Sum.jsx';
+// WRITE TESTS HERE
 
-describe('Sum component', () => {
-  before(() => {
+describe('Page', () => {
+  it('should be visitable', () => {
     cy.visit('/');
-  });
-
-  it('should sum two random numbers in the App', () => {
-    const a = Math.floor(Math.random() * 9);
-    const b = Math.floor(Math.random() * 9);
-    const result = Sum({
-      a,
-      b,
-    }).props.children;
-
-    expect(result)
-      .to.equal(`Sum of ${a} and ${b} is ${a + b}`);
   });
 });
