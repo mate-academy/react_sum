62d8ea725fabe9f1ad8bd64bb1cedef8a214c640
y.kostenko <y.kostenko@mate.academy>
2022-03-28 17:30:26 +0300

    cypress tests added

--- cypress/integration/page.spec.js
+++ cypress/integration/page.spec.js
@@ -1,7 +1,19 @@
-// WRITE TESTS HERE
+import Sum from '../../src/components/Sum.jsx';
 
-describe('Page', () => {
-  it('should be visitable', () => {
+describe('Sum component', () => {
+  before(() => {
     cy.visit('/');
+  });
+
+  it('should sum two random numbers in the App', () => {
+    const a = Math.floor(Math.random() * 9);
+    const b = Math.floor(Math.random() * 9);
+    const result = Sum({
+      a,
+      b,
+    }).props.children;
+
+    expect(result)
+      .to.equal(`Sum of ${a} and ${b} is ${a + b}`);
   });
 });
