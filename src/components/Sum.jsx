2d3dff4cb9e10b8cbe3bdfb44bbd5a75639db3c1
KristinaTar <kristina.tarashchuk@gmail.com>
2022-03-30 21:29:12 +0300

    first commit

---
+++ src/components/Sum.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Sum = props => (
+  <p>
+    {`Sum of ${props.a} and ${props.b} is ${props.a + props.b}`}
+  </p>
+);
+
+Sum.defaultProps = {
+  a: 0,
+  b: 0,
+};
+
+Sum.propTypes = {
+  a: PropTypes.number,
+  b: PropTypes.number,
+};
+
+export default Sum;
