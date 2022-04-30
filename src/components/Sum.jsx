0f70d10ce0036a4c0e8262ce6d1e25ba050bba23
Dima <d.maik2178@gmail.com>
2022-04-30 22:24:26 +0300

    First commit

---
+++ src/components/Sum.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Sum( { a, b } ) {
+
+  return (
+    <p>Sum of {a} and {b} is {a + b}</p>
+  )
+}
+
+Sum.defaultProps = {
+  a: 0,
+  b: 0,
+};
+
+export default Sum;
