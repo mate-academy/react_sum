5cb6317cc20d78f7a5c81327207f63f1a0cdf479
user <user@Igors-MacBook-Air.local>
2022-05-24 18:39:00 +0200

    Fix Sum component

--- src/components/Sum.jsx
+++ src/components/Sum.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export const Sum = ({ a = 0, b = 0 }) => (
+export const Sum = ({ a, b }) => (
   <p>
     {`Sum of ${a} and ${b} is ${a + b}`}
   </p>
 );
 
 Sum.propTypes = {
-  a: PropTypes.number.isRequired,
-  b: PropTypes.number.isRequired,
+  a: PropTypes.number,
+  b: PropTypes.number,
 };
+
+Sum.defaultProps = {
+  a: 0,
+  b: 0
+}
