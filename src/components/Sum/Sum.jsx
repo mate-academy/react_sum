6ca79cd8989efbfd4cc2894a1df24c82571e3623
Aleksandr Khozhainov <khozhainov.aleksandr@gmail.com>
2022-05-24 16:33:49 +0300

    Task Done v1.0

---
+++ src/components/Sum/Sum.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const Sum = ({ a = 0, b = 0 }) => {
+  const result = a + b;
+
+  return (
+    <p>
+      {`Sum of ${a} and ${b} is ${result}`}
+    </p>
+  );
+};
