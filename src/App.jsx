dd860250226c7ca16243c973fc2010f4f65ae487
Alexandra Protyanova <protyanova22@gmail.com>
2022-05-25 14:39:19 +0300

    spaces were added

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,11 @@ import { Sum } from './components/Sum';
 
 const App = () => (
   <>
-    < Sum a = {2}  b = {3}/>
-    < Sum a = {-5}  b = {5}/>
-    < Sum a = {10}/>
-    < Sum b = {5}/>
-    < Sum/>
+    <Sum a={2} b={3} />
+    <Sum a={-5} b={5} />
+    <Sum a={10} />
+    <Sum b={5} />
+    <Sum />
   </>
 );
 
