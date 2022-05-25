290bf55ad821bca622edeeac5425a960e60bd19f
Uliton <x3m@Oleksandrs-MacBook-Pro.local>
2022-05-25 17:29:22 +0200

    fix bug

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,15 @@ import React from 'react';
 import { Sum } from './components/Sum';
 import './App.scss';
 
-const App = () => (
+export const App = () => (
   <>
     <Sum a={2} b={3} />
     <Sum a={-5} b={5} />
-    <Sum a={10} b={0} />
-    <Sum a={0} b={5} />
-    <Sum a={0} b={0} />
+    <Sum a={10} />
+    <Sum b={5} />
+    <Sum />
   </>
+
 );
 
 export default App;
--- src/components/Sum.jsx
+++ src/components/Sum.jsx
@@ -1,1 +1,12 @@
-export const Sum = props => props.a + props.b;
+import React from 'react';
+
+export const Sum = props => (
+  <p>
+    {`Sum of ${props.a} and ${props.b} is ${props.a + props.b}`}
+  </p>
+);
+
+Sum.defaultProps = {
+  a: 0,
+  b: 0,
+};
