417e78d790bad51384e88deddb7d1aae2461df28
Anastasiia Sopotnytska <nastyasopotnytska14@gmail.com>
2022-05-25 17:56:22 +0300

    change default

--- src/components/Sum.jsx
+++ src/components/Sum.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
-export const Sum = ({ a, b }) => (
+export const Sum = ({ a = 0, b = 0 }) => (
   <p>
-    {`Sum of ${a || 0} and ${b || 0} is ${(a || 0) + (b || 0)}`}
+    {`Sum of ${a} and ${b} is ${a + b}`}
   </p>
 );
