3441daa97438dfe990c29180fc979f3fbbc868ed
Batick3737 <antonandriy@ukr.net>
2022-05-25 20:11:58 +0300

    write code

---
+++ src/components/Sum.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export const Sum = ({ a = 0, b = 0 }) => {
+  const sum = a + b;
+
+  return (
+    <p>
+      {' '}
+      Sum of
+      {a}
+      {' '}
+      and
+      {b}
+      {' '}
+      is
+      {sum}
+    </p>
+  );
+};
