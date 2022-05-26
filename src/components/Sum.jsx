66452f6137e33bd28cf447bd0ab006f2d5de3061
Volodymyr <lesksv2000@gmail.com>
2022-05-26 22:00:11 +0300

    get sum fixed props

--- src/components/Sum.jsx
+++ src/components/Sum.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 export const Sum = ({ a = 0, b = 0 }) => (
   <p className="paragraph">
     {`Sum of ${a} and ${b} is ${a + b}`}
   </p>
 );
+Sum.propTypes = {
+  a: PropTypes.string,
+  b: PropTypes.string,
+};
+
+Sum.defaultProps = {
+  a: 'Value: a',
+  b: 'Value: b',
+};
