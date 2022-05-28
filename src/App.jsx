adbf059cf06f35ec931c8241f0097254f4dfa37a
Yaroslav Kvasnytsia <qwerty300067@gmail.com>
2022-05-28 13:44:02 +0300

    react sum2.0

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { Sum } from './components';
 export const App = () => (
   <>
     <Sum a={2} b={3} />
-    <Sum a={5} b={-5} />
+    <Sum a={-5} b={5} />
     <Sum a={10} />
     <Sum b={5} />
     <Sum />
