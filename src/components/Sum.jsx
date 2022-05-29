9449d7bb467ea1e7dcc1fdde31ccecfcb92c9f33
Rashaad Ebrahim <rmebrahim@gmail.com>
2022-05-29 22:21:54 +0200

    Task complete

---
+++ src/components/Sum.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Sum = (props) => {
+  const {
+    a = 0,
+    b = 0,
+  } = props;
+  const total = a + b;
+
+  return (
+    <p>
+      {`Sum of ${a} and ${b} is ${total}`}
+    </p>
+  );
+};
+
+export default Sum;
