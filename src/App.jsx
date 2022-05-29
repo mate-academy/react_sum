845d35e9d9d5f451c69d97d1c327170584a26a17
Rashaad Ebrahim <rmebrahim@gmail.com>
2022-05-29 22:53:11 +0200

    Recommitted task as tests were failing

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,13 @@ import Sum from './components/Sum';
 
 const App = () => (
   <>
+
     <Sum a={2} and b={3} />
     <Sum a={-5} and b={5} />
     <Sum a={10} />
     <Sum b={5} />
     <Sum />
+
   </>
 );
 
