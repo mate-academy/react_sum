e4e245b89e74603948c681a2266309d29251c515
Rashaad Ebrahim <rmebrahim@gmail.com>
2022-05-29 22:57:16 +0200

    Recommitted task as tests were failing

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
 import React from 'react';
 import './App.scss';
-import Sum from './components/Sum';
+import { Sum } from './components';
 
 const App = () => (
   <>
-
     <Sum a={2} and b={3} />
     <Sum a={-5} and b={5} />
     <Sum a={10} />
     <Sum b={5} />
     <Sum />
-
   </>
 );
 
--- src/components/Sum.jsx
+++ src/components/Sum.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Sum = (props) => {
+export const Sum = (props) => {
   const {
     a = 0,
     b = 0,
