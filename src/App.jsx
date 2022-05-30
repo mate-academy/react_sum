e595bd7efffa69c2d2ef6a5e28137a57ab2024fa
MabuzaM <mabuzamfano@gmail.com>
2022-05-30 12:16:46 +0200

    Completed the task

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,34 @@
 import React from 'react';
 import './App.scss';
+import Sum from './components/Sum';
+import SumProps from './components/SumProps';
 
 const App = () => (
   <>
-    <p>Sum of 2 and 3 is 5</p>
-    <p>Sum of -5 and 5 is 0</p>
-    <p>Sum of 10 and 0 is 10</p>
-    <p>Sum of 0 and 5 is 5</p>
-    <p>Sum of 0 and 0 is 0</p>
-    {/* Replace paragraphs with Sum componets */}
-    {/* And remove comment lines :) */}
+    <Sum
+      a={SumProps.sum1.a}
+      b={SumProps.sum1.b}
+    />
+
+    <Sum
+      a={SumProps.sum2.a}
+      b={SumProps.sum2.b}
+    />
+
+    <Sum
+      a={SumProps.sum3.a}
+      b={SumProps.sum3.b}
+    />
+
+    <Sum
+      a={SumProps.sum4.a}
+      b={SumProps.sum4.b}
+    />
+
+    <Sum
+      a={SumProps.sum5.a}
+      b={SumProps.sum5.b}
+    />
   </>
 );
 
---
+++ src/components/SumProps.jsx
@@ -0,0 +1,23 @@
+const SumProps = {
+  sum1: {
+    a: 2,
+    b: 3,
+  },
+
+  sum2: {
+    a: -5,
+    b: 5,
+  },
+
+  sum3: {
+    a: 10,
+  },
+
+  sum4: {
+    b: 5,
+  },
+
+  sum5: {},
+};
+
+export default SumProps;
