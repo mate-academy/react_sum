64481f685bce765fb5ca8b889004fa290422262d
Sardorbek Makhamadaliev <smakhamadaliev8@gmail.com>
2022-05-31 14:42:04 +0200

    react_sum

---
+++ src/components/Sum.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export const Sum = ({ a, b }) => {
+  const res = a + b;
+
+  return (
+    <p>
+      {`Sum of ${a} and ${b} is ${res}`}
+    </p>
+  );
+};
+
+Sum.defaultProps = {
+  a: 0,
+  b: 0,
+};
+
+Sum.propTypes = {
+  a: PropTypes.number,
+  b: PropTypes.number,
+};
+
+export default Sum;
