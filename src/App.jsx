04e8e40f7eed2f0ffb37f4da597df39406b90d33
Marta Fedoryshyn <fedoryshyn.marta@gmail.com>
2022-06-30 15:15:50 +0300

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import './App.scss';
-import { Sum } from './components';
+import { Sum } from './components/Sum';
 
 const App = () => (
   <>
--- src/components/Sum.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export const Sum = ({ a = 0, b = 0 }) => (
-  <p>{`Sum of ${a} and ${b} is ${a + b}`}</p>
-);
