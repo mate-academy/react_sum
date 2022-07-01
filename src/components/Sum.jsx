71e9d2daf52e415207b9e12eea0adc65ac71a071
Mariia Nedilko <mary.nedilko1@gmail.com>
2022-07-01 17:09:52 +0200

    Solution created

--- src/components/Sum.jsx
+++ src/components/Sum.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export const Sum = ({ a = 0, b = 0 }) => (
+export const Sum = ({ a, b }) => (
   <p>
     {`Sum of ${a} and ${b} is ${a + b}`}
   </p>
