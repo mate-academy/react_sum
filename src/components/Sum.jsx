889aa087351d9ab2e28a7e6ea611e8c83ff520e7
Ihor Shynkar <aedricson@gmail.com>
2022-07-01 14:55:58 +0300

    changed component syntax

--- src/components/Sum.jsx
+++ src/components/Sum.jsx
@@ -2,18 +2,6 @@ import React from 'react';
 
 export function Sum({ a = 0, b = 0 }) {
   return (
-    <p>
-      Sum of
-      {' '}
-      {a}
-      {' '}
-      and
-      {' '}
-      {b}
-      {' '}
-      is
-      {' '}
-      {a + b}
-    </p>
+    <p>{`Sum of ${a} and ${b} is ${a + b}`}</p>
   );
 }
