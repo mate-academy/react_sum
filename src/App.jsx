c8d5c9984f42f3e9ed5a7869f2a8e83d98c0b911
2022YEAR <tonnyedgy@gmail.com>
2022-07-01 16:21:58 +0300

    Fix 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import './App.scss';
-import { Sum } from './components/Sum';
+import Sum from './components/Sum';
 
 const App = () => (
   <>
-    {Sum({ a: 2, b: 3 })}
-    {Sum({ a: -5, b: 5 })}
-    {Sum({ a: 10 })}
-    {Sum({ b: 5 })}
-    {Sum({})}
+    <Sum a={2} b={3} />
+    <Sum a={-5} b={5} />
+    <Sum a={10} />
+    <Sum b={5} />
+    <Sum />
   </>
 );
 
