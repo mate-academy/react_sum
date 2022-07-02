b80313222fce383a67b9b87b327bbed492fcff37
Yana Ostapenko <yana.ostapenko@gmail.com>
2022-07-02 16:11:31 +0300

    Solution

---
+++ src/components/Sum.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export function Sum({ a = 0, b = 0 }) {
+  return (
+    <p>
+      Sum of
+      {' '}
+      {a}
+      {' '}
+      and
+      {' '}
+      {b}
+      {' '}
+      is
+      {' '}
+      {a + b}
+    </p>
+  );
+}
