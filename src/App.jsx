69b63830e25845d83cf197bee7268e408a083802
mhrynko <glukym@gmail.com>
2022-07-08 14:38:33 +0300

    fix tests

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const App = () => (
+export const App = () => (
   <>
     <p>Sum of 2 and 3 is 5</p>
     <p>Sum of -5 and 5 is 0</p>
@@ -8,8 +8,6 @@ const App = () => (
     <p>Sum of 0 and 5 is 5</p>
     <p>Sum of 0 and 0 is 0</p>
     {/* Replace paragraphs with Sum componets */}
-    {/* And remove comment lines :) */}
+    {/* And remove commented line lines :) */}
   </>
 );
-
-export default App;
