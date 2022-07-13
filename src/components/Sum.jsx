af7f17e95406ce5c9af549dda29ff18f98878e16
Roman Nikitenko <Lomkaaa@ukr.net>
2022-07-13 21:55:21 +0300

    Task complete

--- src/components/Sum.jsx
+++ src/components/Sum.jsx
@@ -1,1 +1,16 @@
-// export function Sum() {}
+import React from 'react';
+
+export const Sum = ({ a = 0, b = 0 }) => {
+  if (a === undefined && b === undefined) {
+    return (
+      <p>Sum of 0 and 0 is 0</p>
+    );
+  }
+
+  const result = a + b;
+  const resultLine = `Sum of ${a} and ${b} is ${result}`;
+
+  return (
+    <p>{resultLine}</p>
+  );
+};
