b38aa300d32950fd18a908b319593a26eacfd5f6
Dima <d.maik2178@gmail.com>
2022-07-15 16:41:54 +0300

    4

--- src/components/Sum.jsx
+++ src/components/Sum.jsx
@@ -4,13 +4,7 @@ function Sum({ a = 0, b = 0 }) {
   return (
     <p>
       Sum of
-      {' '}
-      {a}
-      {' '}
-      and
-      {b}
-      is
-      {a + b}
+      {`Sum of ${a} and ${b} is ${a + b}`}
     </p>
   );
 }
