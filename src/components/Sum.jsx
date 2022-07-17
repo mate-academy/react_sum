8346b75b79f75260b176e6d1fb0680a7582f1474
Alina Yermolenko <alina.yermolenko27@gmail.com>
2022-07-17 17:41:54 +0300

    done

--- src/components/Sum.jsx
+++ src/components/Sum.jsx
@@ -3,12 +3,7 @@ import React from 'react';
 export function Sum({ a = 0, b = 0 }) {
   return (
     <p>
-      Sum of
-      {a}
-      and
-      {b}
-      is
-      {a + b}
+      Sum of {a} and {b} is {a + b}
     </p>
   );
 }
