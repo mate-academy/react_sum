bfad2211c48364404d5c58f1f4d53cd16354b9b6
Oleg Vetrov <oleg94038@gmail.com>
2022-07-28 16:42:45 +0300

    Added Sum component

--- src/components/Sum.jsx
+++ src/components/Sum.jsx
@@ -1,1 +1,18 @@
-// export function Sum() {}
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export const Sum = ({ a, b }) => {
+  const message = `Sum of ${a} and ${b} is ${a + b}`;
+
+  return (<p>{message}</p>);
+};
+
+Sum.defaultProps = {
+  a: 0,
+  b: 0,
+};
+
+Sum.propTypes = {
+  a: PropTypes.number,
+  b: PropTypes.number,
+};
