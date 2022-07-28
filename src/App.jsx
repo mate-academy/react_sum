2b582581bdd15969f13bf9f794253c1e746e717d
Oleksii Zasiedka <alex.zasedka@gmail.com>
2022-07-28 17:17:22 +0300

    made the tests pass

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Sum } from './components/Sum';
 
 export const App = () => (
   <>
-    <Sum a={2} b={2} />
+    <Sum a={2} b={3} />
     <Sum a={-5} b={5} />
     <Sum a={10} b={0} />
     <Sum a={0} b={5} />
