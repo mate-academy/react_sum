d8fe2f4f01d01a0d8f36d301079c17b133c071e8
lentyaeev <daniilshaliuk@gmail.com>
2022-08-01 18:19:52 +0300

    Fixed

--- src/components/Sum.jsx
+++ src/components/Sum.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
 
-function Sum({ a = 0, b = 0 }) {
+export function Sum({ a = 0, b = 0 }) {
   return (
     <p>
       {`Sum of ${a} and ${b} is ${a + b}`}
     </p>
   );
 }
-
-export default Sum;
