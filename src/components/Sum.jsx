3529bab07722755c092897a4608580fd73a55852
Bogdan Prokopenko <pb.dvlpr@gmail.com>
2022-08-02 15:56:03 +0300

    React Sum

--- src/components/Sum.jsx
+++ src/components/Sum.jsx
@@ -1,1 +1,22 @@
+import React from 'react';
 // export function Sum() {}
+export const Sum = ({ a, b }) => (
+  <p>
+    Sum of
+    {' '}
+    {a}
+    {' '}
+    and
+    {' '}
+    {b}
+    {' '}
+    is
+    {' '}
+    {a + b}
+  </p>
+);
+
+Sum.defaultProps = {
+  a: 0,
+  b: 0,
+};
