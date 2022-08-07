d25ff64e723bd70af61c05bf138f5ecbe8f4d7fc
Stanislav Bzhezinskyi <stasiva28@gmail.com>
2022-08-07 09:49:08 +0300

    some text

---
+++ src/components/Sum/Sum.jsx
@@ -0,0 +1,26 @@
+/* eslint-disable react/no-typos */
+import React from 'react';
+
+export const Sum = ({
+  a,
+  b,
+}) => (
+  <p>
+    Sum of
+    {' '}
+    {a}
+    {' '}
+    and
+    {' '}
+    {b}
+    {' '}
+    is
+    {' '}
+    {a + b}
+  </p>
+);
+
+Sum.defaultProps = {
+  a: 0,
+  b: 0,
+};
