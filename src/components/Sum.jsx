3e0117081d628b2ed9e070c7be0e74e0495dad53
Synytsyn Denys <yungdena54@gmail.com>
2022-08-29 15:23:21 +0300

    React sum

--- src/components/Sum.jsx
+++ src/components/Sum.jsx
@@ -1,1 +1,20 @@
-// export function Sum() {}
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export function Sum({ a, b }) {
+  Sum.propTypes = {
+    a: PropTypes.number,
+    b: PropTypes.number,
+  };
+
+  Sum.defaultProps = {
+    a: 0,
+    b: 0,
+  };
+
+  return (
+    <p>
+      {`Sum of ${a} and ${b} is ${a + b}`}
+    </p>
+  );
+}
