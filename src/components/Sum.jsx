f80647a2e3a59829f807fe2bc6ba3d9be589cc2f
Andriy Kachalkin <kachalkin1306@gmail.com>
2022-08-31 18:32:36 +0300

    Created sum components

--- src/components/Sum.jsx
+++ src/components/Sum.jsx
@@ -1,1 +1,14 @@
-// export function Sum() {}
+import React from 'react';
+
+export function Sum({ firstValue, secondValue }) {
+  const sum = firstValue + secondValue;
+
+  return (
+    <p>{`Sum of ${firstValue} and ${secondValue} is ${sum}`}</p>
+  );
+}
+
+Sum.defaultProps = {
+  firstValue: 0,
+  secondValue: 0,
+};
