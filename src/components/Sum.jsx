e6fa3d2811a554d560afaa3c26d53ed5dc8d6138
Andriy Kachalkin <kachalkin1306@gmail.com>
2022-08-31 19:23:24 +0300

    added property types

--- src/components/Sum.jsx
+++ src/components/Sum.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 export function Sum({ a, b }) {
   const sum = a + b;
@@ -11,4 +12,9 @@ export function Sum({ a, b }) {
 Sum.defaultProps = {
   a: 0,
   b: 0,
+};
+
+Sum.propTypes = {
+  a: PropTypes.number,
+  b: PropTypes.number,
 };
