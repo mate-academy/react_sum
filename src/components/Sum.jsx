e848edb596061645a1a5c0b7d41380c42a3db00b
Igor Bondarenko <igorbondarenko35@gmail.com>
2022-09-05 20:33:56 +0300

    add solution created

--- src/components/Sum.jsx
+++ src/components/Sum.jsx
@@ -1,1 +1,14 @@
-// export function Sum() {}
+import React from 'react';
+
+export function Sum({ a, b }) {
+  return (
+    <p>
+      {`Sum of ${a || 0} and ${b || 0} is ${a + b}`}
+    </p>
+  );
+}
+
+Sum.defaultProps = {
+  a: 0,
+  b: 0,
+};
