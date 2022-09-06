baba2d655f1e71e589d98b527e6bf8cbcf8070e8
Ostap Blystsiv <ostapcr7@gmail.com>
2022-09-06 14:57:50 +0300

    fixes according to comments

--- src/components/Sum.jsx
+++ src/components/Sum.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 function Sum({ a = 0, b = 0 }) {
   return (
     <>
-      {`Sum of ${a} and ${b} is ${a + b}`}
+      <p>
+        {`Sum of ${a} and ${b} is ${a + b}`}
+      </p>
     </>
   );
 }
