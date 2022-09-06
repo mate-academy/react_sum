30c65e0c12c074e9b12ca7d94593bbdef9e30d1f
Svyatoslav <svyatos1337@gmail.com>
2022-09-07 00:25:12 +0300

    Solution

--- src/components/Sum.jsx
+++ src/components/Sum.jsx
@@ -1,1 +1,9 @@
-// export function Sum() {}
+import React from 'react';
+
+export function Sum(props) {
+  const firstNum = props.a ?? 0;
+  const secondNum = props.b ?? 0;
+  const sum = firstNum + secondNum;
+
+  return <p>{`Sum of ${firstNum} and ${secondNum} is ${sum}`}</p>;
+}
