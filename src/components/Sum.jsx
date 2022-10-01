515fc7a47166f4bcbefe7982643ee962ea2cb519
Max Bovtun <maxbovtun@gmail.com>
2022-10-01 17:35:17 +0300

    solution

--- src/components/Sum.jsx
+++ src/components/Sum.jsx
@@ -1,1 +1,9 @@
-// export function Sum() {}
+import React from 'react';
+
+export function Sum({ a, b }) {
+  return (
+    <p>
+      {`Sum of ${a || 0} and ${b || 0} is ${(a || 0) + (b || 0)}`}
+    </p>
+  );
+}
