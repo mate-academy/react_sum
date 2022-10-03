b73bf86fcad7f6348d717715a44c3b1938e72fb4
Ihor Andrianov <andrianoia@gmail.com>
2022-10-03 16:34:58 +0300

    fixed string interpolation

--- src/components/Sum.jsx
+++ src/components/Sum.jsx
@@ -5,17 +5,7 @@ export const Sum = ({ a = 0, b = 0 }) => {
 
   return (
     <p>
-      Sum of
-      {' '}
-      {a}
-      {' '}
-      and
-      {' '}
-      {b}
-      {' '}
-      is
-      {' '}
-      {sum}
+      {`Sum of ${a} and ${b} is ${sum}`}
     </p>
   );
 };
