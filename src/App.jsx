9f11d3a392240a3b388aa9ceb4c9d365844746e0
p-omogit-e <polinakotyrlo@gmail.com>
2022-10-04 11:33:08 +0300

    edited

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,31 @@
 import React from 'react';
 import './App.css';
-import { Sum } from './components/index';
+import { Sum } from './components';
 
 export const App = () => (
   <>
-    <Sum />
+    <Sum
+      a={2}
+      b={3}
+    />
+
+    <br />
+
+    <Sum
+      a={-5}
+      b={5}
+    />
+
+    <br />
+
+    <Sum
+      a={10}
+    />
+
+    <br />
+
+    <Sum
+      b={10}
+    />
   </>
 );
--- src/components/Sum.jsx
+++ src/components/Sum.jsx
@@ -1,7 +1,8 @@
-export function Sum(
+
+export function Sum({
   a = 0,
   b = 0,
-) {
+}) {
   const sum = a + b;
 
   return `Sum of ${a} and ${b} is ${sum}`;
