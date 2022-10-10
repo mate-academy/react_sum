3ba7cf2fc74f6a20a9122d119a4503d4ca3ca31c
Yaroslav Radchuk <105112047+Yaroslav-Radchuk@users.noreply.github.com>
2022-10-10 13:47:01 +0300

    fixed

--- src/components/Sum.jsx
+++ src/components/Sum.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-// import { number } from 'prop-types';
 
 export function Sum({ a, b }) {
   return (
@@ -19,12 +18,3 @@ Sum.propTypes = {
   a: PropTypes.number,
   b: PropTypes.number,
 };
-
-// <>
-// <h1>{`Sum of ${a = 2} and ${b = 3} is ${a + b}`}</h1>
-// <h1>{`Sum of ${a = -5} and ${b = 5} is ${a + b}`}</h1>
-// <h1>{`Sum of ${a = 10} and ${b = 0} is ${a + b}`}</h1>
-// <h1>{`Sum of ${a = 0} and ${b = 5} is ${a + b}`}</h1>
-// <h1>{`Sum of ${a = 0} and ${b = 0} is ${a + b}`}</h1>
-// </>
-// export default Sum;
