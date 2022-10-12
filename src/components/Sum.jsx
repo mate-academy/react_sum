472690aece05beaeacf3f2c3b3bdba1b27bc8f67
tatyana22 <tanya_sobol@ukr.net>
2022-10-12 15:05:29 +0200

    Solution

--- src/components/Sum.jsx
+++ src/components/Sum.jsx
@@ -2,10 +2,15 @@
 import React from 'react';
 
 export const Sum = ({
-  a = 0,
-  b = 0,
+  a,
+  b,
 }) => (
   <p>
     {`Sum of ${a} and ${b} is ${a + b}`}
   </p>
 );
+
+Sum.defaultProps = {
+  a: 0,
+  b: 0
+}
