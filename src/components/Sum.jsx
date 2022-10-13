293c459c96ba1d0f1a7004087acb3e0011244728
Dima Romanenko <demigod177712@gmail.com>
2022-10-13 11:20:59 +0300

    arrow function created

--- src/components/Sum.jsx
+++ src/components/Sum.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
 
-export function Sum({ a = 0, b = 0 }) {
-  return (
-    <p>
-      {`Sum of ${a} and ${b} is ${a + b}`}
-    </p>
-  );
-}
+export const Sum = ({ a = 0, b = 0 }) => (
+  <p>
+    {`Sum of ${a} and ${b} is ${a + b}`}
+  </p>
+);
