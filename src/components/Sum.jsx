eb3380e046a9e9d9449cad1f764b5a119e11bb18
George.kom <komolovua@gmail.com>
2022-10-13 11:43:02 +0300

    Fork problem test

--- src/components/Sum.jsx
+++ src/components/Sum.jsx
@@ -4,10 +4,8 @@ export function Sum({ a = 0, b = 0 }) {
   const sum = a + b;
 
   return (
-    <>
-      <p>
-        {`Sum of ${a} and ${b} is ${sum}`}
-      </p>
-    </>
+    <p>
+      {`Sum of ${a} and ${b} is ${sum}`}
+    </p>
   );
 }
