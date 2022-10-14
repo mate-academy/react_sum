f5fad84ea3feeb8b9c0038198add1d5da5ee0ee1
Boris Kerekesha <kerekesha90@ukr.net>
2022-10-14 11:01:39 +0300

    sum react

--- src/components/Sum.jsx
+++ src/components/Sum.jsx
@@ -2,18 +2,8 @@ import React from 'react';
 
 export const Sum = ({ a, b }) => (
   <p>
-    {
-    `Sum of
-    ${a}
-    and
-    ${b}
-    is
-    ${a + b}`
-    }
+    {`Sum of ${a} and ${b} is ${a + b}`}
   </p>
 );
 
-Sum.defaultProps = {
-  a: 0,
-  b: 0,
-};
+Sum.defaultProps = { a: 0, b: 0 };
