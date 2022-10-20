75c83c9bd8c93ab65ede0f5b5cc7f664436421d4
Oleh Savchuk <sav4uk.oleg@gmail.com>
2022-10-20 23:56:08 +0300

    solution

--- src/components/Sum.jsx
+++ src/components/Sum.jsx
@@ -3,11 +3,7 @@ import React from 'react';
 export function Sum(props) {
   return (
     <p>
-      Sum of
-      {props.a}
-      and {props.b}
-      is
-      {+props.a + +props.b}
+      Sum of {props.a} and {props.b} is {+props.a + +props.b}
     </p>
   );
 }
