15e07b8f114028d660c0a947fc2ae2ec7c7a2066
Eugene P <eugenep@MacBook-Pro.local>
2022-10-31 14:06:20 +0200

    sum0.1

--- src/components/Sum.jsx
+++ src/components/Sum.jsx
@@ -7,38 +7,16 @@ Sum.propTypes = {
 
 };
 
-// works without desctructuring
-// Sum.defaultProps = {
-//   a: 0,ç
-//   b: 0,
-// };
-
 export function Sum({
   a = 0,
   b = 0,
 }) {
   const sum = a + b;
 
-  if (a === 0 && b === 0) {
+  if (sum === 0) {
     <p>
       no params at all
     </p>;
-  }
-
-  if (b === 0) {
-    return (
-      <p>
-        {`Sum of ${a} and ${b} is ${sum}`}
-      </p>
-    );
-  }
-
-  if (a === 0) {
-    return (
-      <p>
-        {`Sum of ${a} and ${b} is ${sum}`}
-      </p>
-    );
   }
 
   return (
