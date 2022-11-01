0bd74a9368954b2cc3b2e71ddb9050e2dd118600
Datsenko Mariia <datsenko.mate@gmail.com>
2022-11-01 20:27:14 +0200

    styles have been added

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,11 @@ import { Sum } from './components/Sum';
 import './App.css';
 
 export const App = () => (
-  <>
+  <div className="content">
     <Sum a={2} b={3} />
     <Sum a={-5} b={5} />
     <Sum a={10} />
     <Sum b={5} />
     <Sum />
-  </>
+  </div>
 );
--- src/components/Sum.jsx
+++ src/components/Sum.jsx
@@ -1,7 +1,29 @@
 import React from 'react';
 
 export const Sum = ({ a = 0, b = 0 }) => (
-  <p>
-    {`Sum of ${a} and ${b} is ${a + b}`}
-  </p>
+  <div className="container">
+    <p>
+      Sum of
+    </p>
+
+    <p className="first">
+      {a}
+    </p>
+
+    <p>
+      and
+    </p>
+
+    <p className="second">
+      {b}
+    </p>
+
+    <p>
+      is
+    </p>
+
+    <p className="sum">
+      {a + b}
+    </p>
+  </div>
 );
