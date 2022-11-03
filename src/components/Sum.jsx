cfb269fe428f05a1fe07a18b5da6b1eee5baa6c7
Yuliia Parashchenko <yuliia.parashchenko@gmail.com>
2022-11-03 02:48:52 +0200

    React sum task solution

--- src/components/Sum.jsx
+++ src/components/Sum.jsx
@@ -1,1 +1,13 @@
-// export function Sum() {}
+import React from 'react';
+
+export function Sum({
+  a = 0,
+  b = 0,
+  text = `Sum of ${a} and ${b} is ${a + b}`,
+}) {
+  return (
+    <p>
+      {text}
+    </p>
+  );
+}
