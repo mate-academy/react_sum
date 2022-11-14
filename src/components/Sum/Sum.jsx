e57a6d8f998207dddbe29f950de4a07756649c4e
Max Fedak <max.fedak@gmail.com>
2022-11-14 16:36:57 +0200

    final

--- src/components/Sum/Sum.jsx
+++ src/components/Sum/Sum.jsx
@@ -7,12 +7,12 @@ export const Sum = ({ a, b }) => (
   </p>
 );
 
-Sum.proptype = {
-  a: PropTypes.number.isRequired,
-  b: PropTypes.number.isRequired,
-};
-
 Sum.defaultProps = {
   a: 0,
   b: 0,
+};
+
+Sum.propTypes = {
+  a: PropTypes.number,
+  b: PropTypes.number,
 };
