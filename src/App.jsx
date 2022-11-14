26330580cfb5dfc9917df1aab8275281000fe248
Max Fedak <max.fedak@gmail.com>
2022-11-14 16:18:38 +0200

    checklist upd

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,10 @@ import { Sum } from './components/Sum';
 
 export const App = () => (
   <>
-    <Sum
-      num1={2}
-      num2={3}
-    />
-    <Sum
-      num1={-5}
-      num2={5}
-    />
-    <Sum
-      num1={10}
-    />
-    <Sum
-      num2={5}
-    />
+    <Sum num1={2} num2={3} />
+    <Sum num1={-5} num2={5} />
+    <Sum num1={10} />
+    <Sum num2={5} />
     <Sum />
   </>
 );
