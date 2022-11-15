1a750c358e9c1f6c0a89723d70b728a4d9525833
Artem66 <melodpi@gmail.com>
2022-11-15 13:05:13 +0000

    refactoring after s/rew

--- src/components/Sum.jsx
+++ src/components/Sum.jsx
@@ -1,12 +1,8 @@
 import React from 'react';
 
-const Sum = ({
+export const Sum = ({
   a = 0,
   b = 0,
 }) => (
-  (
-    <p>{`Sum of ${a} and ${b} is ${a + b}`}</p>
-  )
+  <p>{`Sum of ${a} and ${b} is ${a + b}`}</p>
 );
-
-export { Sum };
