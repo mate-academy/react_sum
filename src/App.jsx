8389783591f8cd3274258a13ae8c02a76ae16f7c
Roman Maksymuk <romamaksymuk@gmail.com>
2022-11-16 10:36:02 +0200

    not done yet

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ export const App = () => (
     <p>Sum of 10 and 0 is 10</p>
     <p>Sum of 0 and 5 is 5</p>
     <p>Sum of 0 and 0 is 0</p>
+
     {/* Replace paragraphs with Sum componets */}
     {/* And remove commented lines :) */}
   </>
