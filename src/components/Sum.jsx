5e48bdc5a3801e8295f170db9a401fcadb01cb52
Roman Maksymuk <romamaksymuk@gmail.com>
2022-11-17 17:08:50 +0200

    prototype added

--- src/components/Sum.jsx
+++ src/components/Sum.jsx
@@ -1,6 +1,12 @@
 
 import React from 'react';
+import PropTypes from 'prop-types';
 
 export const Sum = ({ a = 0, b = 0 }) => (
   <p>{`Sum of ${a} and ${b} is ${a + b}`}</p>
 );
+
+Sum.prototype = {
+  a: PropTypes.number,
+  b: PropTypes.number,
+};
