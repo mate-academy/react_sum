ae35a72268d7f0c59425df97899acbd33ef65096
Ruslan Nedaiborshch <rnedaiborshch@gmail.com>
2022-11-20 09:10:44 +0200

    cod for react sum

--- src/components/Sum.jsx
+++ src/components/Sum.jsx
@@ -1,1 +1,16 @@
-// export function Sum() {}
+import PropTypes from 'prop-types';
+
+export function Sum({ a, b }) {
+  return (`Sum of ${a} and ${b} is ${a + b}`);
+}
+
+Sum.defaultProps = {
+  a: 0,
+  b: 0,
+};
+
+Sum.PropTypes = {
+  Sum: PropTypes.number,
+  a: PropTypes.number,
+  b: PropTypes.number,
+};
