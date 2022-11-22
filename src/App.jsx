30fbefbb8fe2073f1b28500790e3db02825d9fb3
Ruslan Nedaiborshch <rnedaiborshch@gmail.com>
2022-11-22 11:59:14 +0200

    cod react sum after review

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,10 @@ import './App.css';
 
 export const App = () => (
   <>
-    <p>
-      <Sum a={2} b={3} />
-    </p>
-    <p>
-      <Sum a={-5} b={5} />
-    </p>
-    <p>
-      <Sum a={10} />
-    </p>
-    <p>
-      <Sum b={5} />
-    </p>
-    <p>
-      <Sum />
-    </p>
+    <p><Sum a={2} b={3} /></p>
+    <p><Sum a={-5} b={5} /></p>
+    <p><Sum a={10} /></p>
+    <p><Sum b={5} /></p>
+    <p><Sum /></p>
   </>
 );
