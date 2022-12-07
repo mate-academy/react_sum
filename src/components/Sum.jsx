4dc4234bcba54152844fcf9a40fb24f255760c22
zbyn3k <coding707@mailbox.org>
2022-12-07 11:57:10 +0100

    Added the Sum component and its utility file

--- src/components/Sum.jsx
+++ src/components/Sum.jsx
@@ -1,1 +1,9 @@
-// export function Sum() {}
+import React from 'react';
+
+export function Sum(props) {
+  const { a = 0, b = 0 } = props;
+
+  const sum = a + b;
+
+  return <p>{`Sum of ${a} and ${b} is ${sum}`}</p>;
+}
