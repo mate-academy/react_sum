859800e90a8632f42a9e43e3835da50131a0461e
Tetiana Melnyk <uasmiykaua@gmail.com>
2022-12-15 21:50:14 +0100

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,10 @@ import { Sum } from './components/Sum';
 
 export const App = () => (
   <>
-    <p><Sum a={2} b={3} /></p>
-    <p><Sum a={-5} b={5} /></p>
-    <p><Sum a={10} /></p>
-    <p><Sum b={5} /></p>
-    <p><Sum /></p>
+    <Sum a={2} b={3} />
+    <Sum a={-5} b={5} />
+    <Sum a={10} />
+    <Sum b={5} />
+    <Sum />
   </>
 );
