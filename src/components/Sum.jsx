6e2f47bc19fce59c7dbb6973d0c880f5a869b452
OLEXANDRA KOROL <sandrakorol3@gmail.com>
2022-12-22 00:51:50 +0200

    react sum created

--- src/components/Sum.jsx
+++ src/components/Sum.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 
-const Sum = ({ a, b }) => {
-  return (
-    <p>
-      Sum of {a} and {b} is {a + b}
-    </p>
-  )
-};
+const Sum = ({ a, b }) => (
+  <p>
+    Sum of
+    {' '}
+    {a}
+    {' '}
+    and
+    {' '}
+    {b}
+    {' '}
+    is
+    {' '}
+    {a + b}
+  </p>
+);
 
 Sum.defaultProps = {
   a: 0,
