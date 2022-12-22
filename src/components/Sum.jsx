58e73532adc61a066f0245214e745db8d1987d62
Mykhailyk Vadym <ihappyspam@gmail.com>
2022-12-22 17:34:02 +0200

    changed export method in Sum.jsx

--- src/components/Sum.jsx
+++ src/components/Sum.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
 
-const Sum = ({ a = 0, b = 0 }) => (
+export const Sum = ({ a = 0, b = 0 }) => (
   <p>
     {`Sum of ${a} and ${b} is ${a + b}`}
   </p>
 );
-
-export default Sum;
