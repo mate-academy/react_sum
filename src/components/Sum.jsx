ffb99bace13740674782a8057df76da9821a7d76
Andrii Petrinko <apetrinko@gmail.com>
2022-12-22 20:05:25 +0200

    forgot to run "npm test" and put "return" in the function))

--- src/components/Sum.jsx
+++ src/components/Sum.jsx
@@ -1,9 +1,9 @@
-import React from "react"
+import React from 'react';
 
-export const Sum =({a = 0, b = 0}) => {
+export const Sum = ({ a = 0, b = 0 }) => {
   return (
     <p>
-      `Sum of ${a} and ${b} is {a + b}`
+      {`Sum of ${a} and ${b} is ${a + b}`}
     </p>
   )
-}
+};
