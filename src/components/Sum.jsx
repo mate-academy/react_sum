891304eb28fbce57b369019673861708387972c9
Olena Yanovska <alena.yn.adw@gmail.com>
2022-12-23 12:48:08 +0200

    fixed mistakes

--- src/components/Sum.jsx
+++ src/components/Sum.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
-export const Sum = ({ a = 0, b = 0 }) => (
-  <p>
-    {`Sum of ${a} and ${b} is ${a + b}`}
-  </p>
-);
+export const Sum = (props) => {
+  const { a = 0, b = 0 } = props;
+
+  return (
+    <p>
+      {`Sum of ${a} and ${b} is ${a + b}`}
+    </p>
+  );
+};
