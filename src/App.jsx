081d70f49656efd0003549c95b522ac1bc2071bc
Danylo Novytskyi <ostroverkhov.novytskyi.danylo@gmail.com>
2022-12-26 01:15:14 +0200

    was brought into line with the checklist

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,9 @@ import './App.css';
 
 export const App = () => (
   <>
-    <Sum
-      a={2}
-      b={3}
-    />
+    <Sum a={2} b={3} />
 
-    <Sum
-      a={-5}
-      b={5}
-    />
+    <Sum a={-5} b={5} />
 
     <Sum a={10} />
 
