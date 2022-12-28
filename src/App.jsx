0ebe97deb83bd718412b8a5943552fed25b188c6
Dmytro Kravchenko <dmytro318@gmail.com>
2022-12-28 21:50:08 +0200

    changed variable value

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { Sum } from './components';
 
 export const App = () => (
   <>
-    <Sum a={2} b={5} />
+    <Sum a={2} b={3} />
     <Sum a={-5} b={5} />
     <Sum a={10} b={0} />
     <Sum a={0} b={5} />
