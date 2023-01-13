5bcc29a3cbf9291fa6ec5b5e8366625ad6426121
miamarichka <ilchyshyn.marichka@gmail.com>
2023-01-13 17:38:44 +0300

    solution

--- src/components/Sum.jsx
+++ src/components/Sum.jsx
@@ -1,1 +1,19 @@
-// export function Sum() {}
+/* eslint-disable react/jsx-one-expression-per-line */
+import { number } from 'prop-types';
+
+export function Sum({ a, b }) {
+  return (
+        // eslint-disable-next-line react/jsx-indent, react/react-in-jsx-scope
+        <p> Sum of {a} and {b} is {a + b} </p>
+  );
+}
+
+Sum.propTypes = {
+  a: number,
+  b: number,
+};
+
+Sum.defaultProps = {
+  a: 0,
+  b: 0,
+};
