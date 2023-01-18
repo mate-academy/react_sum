baef855fd5f91b9bb8abb7fa57705c2c0197294c
Konstantin Burnazaki <kostas.burnazaki@gmail.com>
2023-01-18 11:50:13 +0200

    fixed as per comments

--- src/components/Sum.jsx
+++ src/components/Sum.jsx
@@ -1,20 +1,14 @@
 import React from 'react';
 
-export const Sum = ({ a, b }) => (
-  <p>
-    Sum of
-    {' '}
-    {a}
-    {' '}
-    and
-    {' '}
-    {b}
-    {' '}
-    is
-    {' '}
-    {a + b}
-  </p>
-);
+export const Sum = ({ a, b }) => {
+  const res = `Sum of ${a} and ${b} is ${a + b}`;
+
+  return (
+    <p>
+      {res}
+    </p>
+  );
+};
 
 Sum.defaultProps = {
   a: 0,
