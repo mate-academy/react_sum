3f85b563bb8793e83b06cc6ebc84830592c7cc34
Tretiak Serhii <tretyak8936@gmail.com>
2023-01-24 15:21:58 +0200

    added types

--- src/components/Sum.jsx
+++ src/components/Sum.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 export const Sum = ({ a = 0, b = 0 }) => (
   <p className="Sum">
     {`Sum of ${a} and ${b} is ${a + b}`}
   </p>
 );
+
+Sum.propTypes = {
+  a: PropTypes.number.isRequired,
+  b: PropTypes.number.isRequired,
+};
