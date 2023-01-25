f3e2d9e263a3cd3cc41837a9aa1f297b0a80ec08
Vasiliy22 <hitretsukrnet@gmail.com>
2023-01-25 15:57:11 +0200

    Solves

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import { Sum } from './components/Sum';
 
 export const App = () => (
   <>
@@ -8,7 +9,11 @@ export const App = () => (
     <p>Sum of 10 and 0 is 10</p>
     <p>Sum of 0 and 5 is 5</p>
     <p>Sum of 0 and 0 is 0</p>
-    {/* Replace paragraphs with Sum componets */}
-    {/* And remove commented lines :) */}
+    <Sum a={10} b={7} />
+    <Sum a={6} b={-4} />
+    <Sum a={-40} b={-2} />
+    <Sum a={6} />
+    <Sum b={12} />
+    <Sum />
   </>
 );
