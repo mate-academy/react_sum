a44afeb0be9f46ff6f97ba310459d422be2dff1a
Vasiliy22 <hitretsukrnet@gmail.com>
2023-01-26 17:58:31 +0200

    fix indents

--- src/components/Sum.jsx
+++ src/components/Sum.jsx
@@ -1,10 +1,6 @@
 import React from 'react';
 
-export function Sum({ a, b }) {
-  return (
-    <p> Sum of {a} and {b} is {a + b}</p>
-  );
-}
+export const Sum = ({ a, b }) => (<p>Sum of {a} and {b} is {a + b}</p>);
 
 Sum.defaultProps = {
   a: 0,
