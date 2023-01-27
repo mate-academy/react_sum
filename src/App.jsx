f32b214af7197c008c72094d8a24de74e1e45fed
Vasiliy22 <hitretsukrnet@gmail.com>
2023-01-27 17:43:36 +0200

    change paragraphs

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import { Sum } from './components/Sum';
 
 export const App = () => (
   <>
+    <p>Sum of 2 and 3 is 5</p>
+    <p>Sum of -5 and 5 is 0</p>
     <Sum a={10} b={7} />
     <Sum a={6} b={-4} />
     <Sum a={-40} b={-2} />
