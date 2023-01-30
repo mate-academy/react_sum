710397a15f131532332de3a615742e246c8b4344
Vasiliy22 <hitretsukrnet@gmail.com>
2023-01-30 09:58:52 +0200

    fix all

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,10 @@ import { Sum } from './components/Sum';
 
 export const App = () => (
   <>
-    <p>Sum of 2 and 3 is 5</p>
-    <p>Sum of -5 and 5 is 0</p>
-    <Sum a={10} b={7} />
-    <Sum a={6} b={-4} />
-    <Sum a={-40} b={-2} />
-    <Sum a={6} />
-    <Sum b={12} />
+    <Sum a={2} b={3} />
+    <Sum a={-5} b={5} />
+    <Sum a={10} />
+    <Sum b={5} />
     <Sum />
   </>
 );
--- src/components/Sum.jsx
+++ src/components/Sum.jsx
@@ -1,16 +1,7 @@
 import React from 'react';
 
 export const Sum = ({ a, b }) => (
-  <p>
-    Sum of
-    {a}
-    {' '}
-    and
-    {b}
-    {' '}
-    is
-    {a + b}
-  </p>
+  <p>{`Sum of ${a} and ${b} is ${a + b}`}</p>
 );
 
 Sum.defaultProps = {
