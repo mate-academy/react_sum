d40ae14591475a8d217ece421ed8b5ae127bb7e0
Karadzha Mykola <nicholaskaradzha@gmail.com>
2023-02-05 20:01:04 +0000

    react sum task completed

--- src/components/Sum.jsx
+++ src/components/Sum.jsx
@@ -1,1 +1,9 @@
-// export function Sum() {}
+/* eslint-disable react/jsx-one-expression-per-line */
+import React from 'react';
+
+export const Sum = ({ a, b }) => <p>Sum of {a} and {b} is {a + b}</p>;
+
+Sum.defaultProps = {
+  a: 0,
+  b: 0,
+};
