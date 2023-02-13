253b789fb666112e9ad6bcc3acb3986358341952
Kate <ekaterina2002aleks@gmail.com>
2023-02-13 22:23:02 +0200

    Solution

--- src/components/Sum.jsx
+++ src/components/Sum.jsx
@@ -1,1 +1,21 @@
-// export function Sum() {}
+import React from 'react';
+
+export function Sum({ a = 0, b = 0 }) {
+  const space = ' ';
+
+  return (
+    <p>
+      Sum of
+      {space}
+      {a}
+      {space}
+      and
+      {space}
+      {b}
+      {space}
+      is
+      {space}
+      {a + b}
+    </p>
+  );
+}
