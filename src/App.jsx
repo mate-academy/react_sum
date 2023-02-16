5ac35dd246c1d5841476e9bc301c5263496a5fab
Peti51 <galpeter31@gmail.com>
2023-02-16 10:40:13 +0100

    commit message

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import './App.css';
 
-export const App = () => (
+export const App = ({ a, b }) => (
   <>
-    <p>Sum of 2 and 3 is 5</p>
-    <p>Sum of -5 and 5 is 0</p>
-    <p>Sum of 10 and 0 is 10</p>
-    <p>Sum of 0 and 5 is 5</p>
-    <p>Sum of 0 and 0 is 0</p>
-    {/* Replace paragraphs with Sum componets */}
-    {/* And remove commented lines :) */}
+    <p>
+      Sum of
+      {' '}
+      {a}
+      {' '}
+      and
+      {' '}
+      {b}
+      {' '}
+      is
+      {' '}
+      {a + b}
+      {' '}
+    </p>
   </>
 );
--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,64 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App';
 
+export const numbers1 = {
+  a: 2,
+  b: 3,
+};
+
+export const numbers2 = {
+  a: -5,
+  b: 5,
+};
+
+export const numbers3 = {
+  a: 10,
+  b: 0,
+};
+
+export const numbers4 = {
+  a: 0,
+  b: 5,
+};
+
+export const numbers5 = {
+  a: 0,
+  b: 0,
+};
+
 ReactDOM.render(
-  <App />,
+  <div>
+    <App {...numbers1} />
+    <App {...numbers2} />
+    <App {...numbers3} />
+    <App {...numbers4} />
+    <App {...numbers5} />
+  </div>,
+
   document.getElementById('root'),
 );
+
+numbers1.defaultProps = {
+  a: 0,
+  b: 0,
+};
+
+numbers2.defaultProps = {
+  a: 0,
+  b: 0,
+};
+
+numbers3.defaultProps = {
+  a: 0,
+  b: 0,
+};
+
+numbers4.defaultProps = {
+  a: 0,
+  b: 0,
+};
+
+numbers5.defaultProps = {
+  a: 0,
+  b: 0,
+};
