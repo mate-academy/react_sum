3930f4a5c4f590fb5c631403d713d5f30ed89d84
Eugene P <eugenep@MacBook-Pro.local>
2023-02-17 18:30:19 +0200

    react_sum_v0.2

--- src/components/Sum.jsx
+++ src/components/Sum.jsx
@@ -1,24 +1,9 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-
-Sum.propTypes = {
-  a: PropTypes.number,
-  b: PropTypes.number,
-
-};
 
 export function Sum({
   a = 0,
   b = 0,
 }) {
-  const sum = a + b;
-
-  if (sum === 0) {
-    <p>
-      no params at all
-    </p>;
-  }
-
   return (
     <p>
       {`Sum of ${a} and ${b} is ${a + b}`}
