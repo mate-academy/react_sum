ee34ec4849cb065ad7c005bb21f053d7513cbb7a
Evgen Maximov <evgen@debian-BULLSEYE-live-builder-AMD64>
2023-02-20 01:05:28 +0200

    Solution+

--- src/components/Sum.jsx
+++ src/components/Sum.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Sum = ({ a, b }) => (
+export const Sum = ({ a, b }) => (
   <p>
     {`Sum of ${a} and ${b} is ${a + b}`}
   </p>
@@ -10,5 +10,3 @@ Sum.defaultProps = {
   a: 0,
   b: 0,
 };
-
-export default Sum;
