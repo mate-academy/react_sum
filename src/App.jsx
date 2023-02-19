b21e55ba80a4d803705241f8940911e80d30c63e
Peti51 <galpeter31@gmail.com>
2023-02-19 14:41:39 +0100

    commit message

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import Sum from './components/Sum';
 
-export const App = () => (
+const App = () => (
   <>
     <Sum a={2} b={3} />
     <Sum a={-5} b={5} />
@@ -11,3 +11,5 @@ export const App = () => (
     <Sum a={0} b={0} />
   </>
 );
+
+export default App;
--- src/components/Sum.jsx
+++ src/components/Sum.jsx
@@ -3,11 +3,11 @@ import React from 'react';
 const Sum = ({ a = 0, b = 0 }) => {
   const result = a + b;
 
-    <>
-      <p>
-        {`Sum of ${a} and ${b} is ${result}`}
-      </p>
-    </>;
+  return (
+    <p>
+      {`Sum of ${a} and ${b} is ${result}`}
+    </p>
+  );
 };
 
 export default Sum;
