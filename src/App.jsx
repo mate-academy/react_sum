2c6fe63874309496bbf22308fe2264e1fdad7cb3
«ifUride» <ifuride@gmail.com>
2023-02-21 09:26:46 +0100

    Display of Sum component in one line

--- src/App.jsx
+++ src/App.jsx
@@ -5,24 +5,10 @@ import './App.css';
 
 export const App = () => (
   <>
-    <Sum
-      a={2}
-      b={3}
-    />
-
-    <Sum
-      a={-5}
-      b={5}
-    />
-
-    <Sum
-      a={10}
-    />
-
-    <Sum
-      b={5}
-    />
-
+    <Sum a={2} b={3} />
+    <Sum a={-5} b={5} />
+    <Sum a={10} />
+    <Sum b={5} />
     <Sum />
   </>
 );
