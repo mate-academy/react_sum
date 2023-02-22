42bc88420b2743e3e3015ff1a2f72c5e9eb2422c
anna-tovkailo <babylike24@gmail.com>
2023-02-22 21:05:34 +0100

    solution2

--- src/components/Sum.jsx
+++ src/components/Sum.jsx
@@ -3,12 +3,15 @@ import React from 'react';
 export const Sum = ({ a = 0, b = 0 }) => (
   <p>
     Sum of
+    {' '}
     {a}
     {' '}
     and
+    {' '}
     {b}
     {' '}
     is
+    {' '}
     {a + b}
   </p>
 );
