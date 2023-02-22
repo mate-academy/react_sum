77ee576a4adc98036029f3b062ebe89a8475ab32
Vadym Prydatok <vadymprydatok@gmail.com>
2023-02-22 18:20:40 +0200

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -5,23 +5,13 @@ import './App.css';
 
 export const App = () => (
   <>
-    <Sum
-      a={2}
-      b={3}
-    />
+    <Sum a={2} b={3} />
 
-    <Sum
-      a={-5}
-      b={5}
-    />
+    <Sum a={-5} b={5} />
 
-    <Sum
-      a={10}
-    />
+    <Sum a={10} />
 
-    <Sum
-      b={5}
-    />
+    <Sum b={5} />
 
     <Sum />
   </>
