0f076ab4fb9d2361cbc9cd28196c52d750a29da9
yandapanda19 <i.am.kitiket@gmail.com>
2023-03-02 02:56:05 +0200

    add solution

--- src/components/Sum.jsx
+++ src/components/Sum.jsx
@@ -1,1 +1,15 @@
-// export function Sum() {}
+import React from 'react';
+
+export function Sum({ a, b }) {
+  return (
+    <p>
+      {/* eslint-disable-next-line react/jsx-one-expression-per-line */}
+      Sum of {a} and {b} is {a + b}
+    </p>
+  );
+}
+
+Sum.defaultProps = {
+  a: 0,
+  b: 0,
+};
