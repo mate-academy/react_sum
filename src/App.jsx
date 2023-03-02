60241f409d6e1c17af43fd38211057e6b58d83f1
Kyrychenko Illia <i.kirichenko160804@gmail.com>
2023-03-02 11:37:16 +0200

    fixed code style

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,8 @@ import { Sum } from './components/Sum';
 
 export const App = () => (
   <>
-    <Sum
-      a={2}
-      b={3}
-    />
-
-    <Sum
-      a={-5}
-      b={5}
-    />
-
+    <Sum a={2} b={3} />
+    <Sum a={-5} b={5} />
     <Sum a={10} />
     <Sum b={5} />
     <Sum />
