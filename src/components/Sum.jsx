85584d29f76bc73c69f924225873f1f9c01c8693
Ashton <mfckr.x@gmail.com>
2023-03-05 13:32:47 +0000

    Update src/components/Sum.jsx
    Co-authored-by: Oleksandr Polotniuk <96409563+polosanya@users.noreply.github.com>

--- src/components/Sum.jsx
+++ src/components/Sum.jsx
@@ -2,6 +2,6 @@ import React from 'react';
 
 export const Sum = ({ a = 0, b = 0 }) => (
   <p>
-    {`Sum of ${a} and ${b} is ${Number(a) + Number(b)}`}
+    {`Sum of ${a} and ${b} is ${a + b}`}
   </p>
 );
