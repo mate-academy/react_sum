4aa2763559283218997c5855dde383e5c4ab95ac
Volodymyr <vovannty@ukr.net>
2023-03-06 16:54:24 +0200

    fix

--- src/components/Sum.jsx
+++ src/components/Sum.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
-export const Sum = ({ a = 0, b = 0 }) => {
-  <p>
-    {`Sum of ${a} and ${b} is ${a + b}`}
-  </p>;
-};
+export function Sum({ a = 0, b = 0 }) {
+  return (
+    <p>{`Sum of ${a} and ${b} is ${a + b}`}</p>
+  );
+}
