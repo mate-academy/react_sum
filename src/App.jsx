29d809c224b0cb3d1add69f46e2d510e01378aca
Gabriella135 <gabriella.ivanjuk@gmail.com>
2023-03-07 21:50:14 +0200

    solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import { Sum } from './components/Sum';
 
 export const App = () => (
   <>
@@ -9,10 +10,4 @@ export const App = () => (
     <Sum a={0} b={5} />
     <Sum a={0} b={0} />
   </>
-);
-
-const Sum = ({ a = 0, b = 0 }) => (
-  <p>
-    {`Sum of ${a} and ${b} is ${a + b}`}
-  </p>
 );
