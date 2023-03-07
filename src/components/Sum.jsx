65c18bf7cd2cdaf71fd2b5f1b98afc366f2677bf
christian m wolf <108151633+devWizHub@users.noreply.github.com>
2023-03-07 20:44:01 +0100

    add task solution

--- src/components/Sum.jsx
+++ src/components/Sum.jsx
@@ -3,8 +3,7 @@ import React from 'react';
 export function Sum({ a, b }) {
   return (
     <p>
-      {`Sum of ${a} and ${b} is `}
-      {a + b}
+      {`Sum of ${a} and ${b} is {a + b}`}
     </p>
   );
 }
