1298f2fa8c1ec9f1037e39d9a215fa702acdb740
Anna Akhalmosulishvili <akhala33@gmail.com>
2023-03-11 16:45:09 +0200

    solution

--- src/components/Sum.jsx
+++ src/components/Sum.jsx
@@ -1,1 +1,22 @@
-// export function Sum() {}
+import React from 'react';
+
+export function Sum(props = 0) {
+  const aNum = +props.a || 0;
+  const bNum = +props.b || 0;
+
+  return (
+    <p>
+      Sum of
+      {' '}
+      {aNum}
+      {' '}
+      and
+      {' '}
+      {bNum}
+      {' '}
+      is
+      {' '}
+      {(aNum + bNum)}
+    </p>
+  );
+}
