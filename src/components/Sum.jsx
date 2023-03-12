20a58a1f5cc76ff5decf3af175cb8d6bcdeba35b
Степан <stepan12@MacBook-Air-Stepan.local>
2023-03-12 20:50:41 +0200

    sum done

--- src/components/Sum.jsx
+++ src/components/Sum.jsx
@@ -1,1 +1,11 @@
 // export function Sum() {}
+import React from 'react';
+
+export const Sum = ({ a, b }) => (
+  <p>{`Sum of ${a} and ${b} is ${a + b}`}</p>
+);
+
+Sum.defaultProps = {
+  a: 0,
+  b: 0,
+};
