de7fc953892de4fd4eacd0f62bd0198fc6b7355f
anton_chornobai <antonchornobajj@gmail.com>
2023-03-13 13:11:59 +0100

    task was done

--- src/components/Sum.jsx
+++ src/components/Sum.jsx
@@ -1,1 +1,19 @@
-// export function Sum() {}
+import React from 'react';
+
+export default function Sum({ a = 0, b = 0 }) {
+  return (
+    <h1>
+      Sum of
+      {' '}
+      {a}
+      {' '}
+      and
+      {' '}
+      {b}
+      {' '}
+      is
+      {' '}
+      {a + b}
+    </h1>
+  );
+}
