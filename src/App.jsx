6fabee89ed449ecaa1811956508f63af917c3962
Bozhena Bodnar <bozhena2705bodnar@gmail.com>
2023-03-13 13:37:01 +0100

    fix code by checklist

--- src/App.jsx
+++ src/App.jsx
@@ -9,19 +9,10 @@ export const App = () => (
     <p>Sum of 10 and 0 is 10</p>
     <p>Sum of 0 and 5 is 5</p>
     <p>Sum of 0 and 0 is 0</p>
-    <Sum
-      a={2}
-      b={3}
-    />
-    <Sum
-      a={-5}
-    />
-    <Sum
-      a={10}
-    />
-    <Sum
-      b={5}
-    />
+    <Sum a={2} b={3} />
+    <Sum a={-5} />
+    <Sum a={10} />
+    <Sum b={5} />
     <Sum />
   </>
 );
