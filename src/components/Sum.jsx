31b8fa06966c2c30307b7627bf890cf894288d4c
Petro Lozynskyi <petrolozynskyi01@gmail.com>
2023-03-21 12:22:31 +0200

    update

--- src/components/Sum.jsx
+++ src/components/Sum.jsx
@@ -1,10 +1,8 @@
 import React from 'react';
 // export function Sum() {}
 
-export function Sum({ a = 0, b = 0 }) {
-  return (
-    <p>
-      {`Sum of ${a} and ${b} is ${a + b}`}
-    </p>
-  );
-}
+export const Sum = ({ a = 0, b = 0 }) => (
+  <p>
+    {`Sum of ${a} and ${b} is ${a + b}`}
+  </p>
+);
