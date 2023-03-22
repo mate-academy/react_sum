ba6f0192220ed4a5319fdf2e689052d7036191f0
Mykola Butylkov <kokos.ibo.58@gmail.com>
2023-03-22 13:32:07 +0200

    Solution-2

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,10 @@ import { Sum } from './components/Sum';
 
 export const App = () => (
   <>
-    <p>{`Sum of 2 and 3 is ${Sum(2, 3)}`}</p>
-    <p>{`Sum of -5 and 5 is ${Sum(-5, 5)}`}</p>
-    <p>{`Sum of 10 and 0 is ${Sum(10)}`}</p>
-    <p>{`Sum of 0 and 5 is ${Sum(5)}`}</p>
-    <p>{`Sum of 0 and 0 is ${Sum()}`}</p>
+    <p><Sum a={2} b={3} /></p>
+    <p><Sum a={-5} b={5} /></p>
+    <p><Sum a={10} /></p>
+    <p><Sum b={3} /></p>
+    <p><Sum /></p>
   </>
 );
--- src/components/Sum.jsx
+++ src/components/Sum.jsx
@@ -1,3 +1,9 @@
-export function Sum(a = 0, b = 0) {
-  return a + b;
+import React from 'react';
+
+export function Sum({ a = 0, b = 0 }) {
+  return (
+    <p>
+      {`Sum of ${a} and ${b} is ${a + b}`}
+    </p>
+  );
 }
