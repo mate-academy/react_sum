66860ef5573aa8cc45ff54bf5c0d320c5e545185
Nazar Vorona <voron1765@gmail.com>
2023-03-26 02:47:15 +0200

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -4,31 +4,14 @@ import { Sum } from './components/Sum';
 
 export const App = () => (
   <>
+    <Sum a={2} b={3} />
 
-    <Sum
-      a={2}
-      b={3}
-    />
+    <Sum a={-5} b={5} />
 
-    <Sum
-      a={-5}
-      b={5}
-    />
+    <Sum a={10} b={0} />
 
-    <Sum
-      a={10}
-      b={0}
-    />
+    <Sum a={0} b={5} />
 
-    <Sum
-      a={0}
-      b={5}
-    />
-
-    <Sum
-      a={0}
-      b={5}
-    />
-
+    <Sum a={0} b={5} />
   </>
 );
