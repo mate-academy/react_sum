bd11b58af472a09a8ac29909ba7468e700e7105f
Nazarii Chepil <nazzikk1998@gmail.com>
2023-04-09 14:38:00 +0300

    solution

--- src/components/Sum.jsx
+++ src/components/Sum.jsx
@@ -1,1 +1,13 @@
-// export function Sum() {}
+import React from 'react';
+
+export function Sum(props) {
+  const a = props.a ? +props.a : 0;
+  const b = props.b ? +props.b : 0;
+  const sum = a + b;
+
+  return (
+    <p>
+      {`Sum of ${a} and ${b} is ${sum}`}
+    </p>
+  );
+}
