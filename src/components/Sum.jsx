8bcce1cc47e7824fd109facaee4d038c78899fe8
zynkd <coding707@mailbox.org>
2023-04-09 15:39:21 +0200

    Get props directly via object destructuring & add default props

--- src/components/Sum.jsx
+++ src/components/Sum.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 
-export function Sum(props) {
-  const { a = 0, b = 0 } = props;
-
+export function Sum({ a, b }) {
   const sum = a + b;
 
   return <p>{`Sum of ${a} and ${b} is ${sum}`}</p>;
 }
+
+Sum.defaultProps = {
+  a: 0,
+  b: 0,
+};
