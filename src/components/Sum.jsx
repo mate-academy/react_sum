40470a6c93c10bc6c71816de846987a24e4993f6
Vyacheslav Stolyarchuk <haunt.beta@gmail.com>
2023-04-12 15:52:49 +0300

    fix code style and import method

--- src/components/Sum.jsx
+++ src/components/Sum.jsx
@@ -1,13 +1,5 @@
 import React from 'react';
 
-function Sum({ a = 0, b = 0 }) {
-  return (
-    <>
-      <p>
-        {`Sum of ${a} and ${b} is ${a + b}`}
-      </p>
-    </>
-  );
-}
-
-export default Sum;
+export const Sum = ({ a = 0, b = 0 }) => (
+  <p>{`Sum of ${a} and ${b} is ${a + b}`}</p>
+);
