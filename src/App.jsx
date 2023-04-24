3fd56a17fa90082648793f08c6dd52985a7dedaf
Ilia Kolesnik <koleshnik160799@gmail.com>
2023-04-24 23:29:28 +0300

    Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import './App.css';
 
+function Paragraph({ a = 0, b = 0 }) {
+  const text = `Sum of ${a} and ${b}  is ${a + b}`;
+
+  return (
+    <p>{ text }</p>
+  );
+}
+
 export const App = () => (
   <>
-    <p>Sum of 2 and 3 is 5</p>
-    <p>Sum of -5 and 5 is 0</p>
-    <p>Sum of 10 and 0 is 10</p>
-    <p>Sum of 0 and 5 is 5</p>
-    <p>Sum of 0 and 0 is 0</p>
-    {/* Replace paragraphs with Sum componets */}
-    {/* And remove commented lines :) */}
+    <Paragraph a={2} b={3} />
+    <Paragraph a={-5} b={5} />
+    <Paragraph a={10} />
+    <Paragraph b={5} />
+    <Paragraph />
   </>
 );
