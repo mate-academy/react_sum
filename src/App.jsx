37c03deb16b4de80e1ca4d77dbbfd507720855ab
kq333 <afugowski@yandex.com>
2023-04-27 11:09:59 +0200

    fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,13 @@
 import React from 'react';
 import './App.css';
-import Sum from './components/Sum';
+import { Sum } from './components/Sum';
 
 export const App = () => (
   <>
-    <Sum
-      a={2}
-      b={3}
-    />
-    <Sum
-      a={-5}
-      b={5}
-    />
-    <Sum
-      a={10}
-      b={10}
-    />
-    <Sum
-      a={0}
-      b={5}
-    />
-    <Sum
-      a={0}
-      b={0}
-    />
+    <Sum a={2} b={3} />
+    <Sum a={-5} b={5} />
+    <Sum a={10} b={10} />
+    <Sum a={0} b={5} />
+    <Sum a={0} b={0} />
   </>
 );
