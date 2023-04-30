4d7871ba147fd83090ba9f28a10070ebc98a38d6
Natali <swetswet1234@gmail.com>
2023-04-30 11:59:16 +0300

    develop

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,8 @@ export const App = () => (
   <>
     <Sum a={2} b={3} />
     <Sum a={-5} b={5} />
-    <Sum a={10} b={undefined} />
-    <Sum a={undefined} b={5} />
-    <Sum a={undefined} b={undefined} />
+    <Sum a={10} />
+    <Sum b={5} />
+    <Sum />
   </>
 );
