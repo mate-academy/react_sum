846ac4ab811ad1dab2e4686a37c903e638f77460
unknown <vorobetst777@gmail.com>
2023-05-04 22:39:46 +0300

    Solution

--- src/components/Sum.jsx
+++ src/components/Sum.jsx
@@ -1,1 +1,20 @@
-// export function Sum() {}
+export function Sum() {
+  return (
+    <>
+      <Paragraph a = {2} b = {3} />
+      <Paragraph a = {-5} b = {5} />
+      <Paragraph a = {10}/>
+      <Paragraph b = {5} />
+      <Paragraph />
+    </>
+  )
+}
+
+const Paragraph = ({ a, b }) => {
+  return <p>Sum of {a} and {b} is {a + b}</p>
+}
+
+Paragraph.defaultProps = {
+  a: 0,
+  b: 0,
+}
