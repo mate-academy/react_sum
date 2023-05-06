8a1ccebea3a432eab5b058aedcc4b0430e9b57bd
unknown <vorobetst777@gmail.com>
2023-05-06 21:00:47 +0300

    Fixed comments

--- src/components/Sum.jsx
+++ src/components/Sum.jsx
@@ -1,20 +1,14 @@
-export function Sum() {
+import React from 'react';
+
+export function Sum({ a, b }) {
   return (
     <>
-      <Paragraph a = {2} b = {3} />
-      <Paragraph a = {-5} b = {5} />
-      <Paragraph a = {10}/>
-      <Paragraph b = {5} />
-      <Paragraph />
+      <p>{`Sum of ${a} and ${b} is ${a + b}`}</p>
     </>
-  )
+  );
 }
 
-const Paragraph = ({ a, b }) => {
-  return <p>Sum of {a} and {b} is {a + b}</p>
-}
-
-Paragraph.defaultProps = {
+Sum.defaultProps = {
   a: 0,
   b: 0,
-}
+};
