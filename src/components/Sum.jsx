de8503689c6b33d071f614934bc7b98de6821d96
Olena Shevchenko <shevochkas@gmail.com>
2023-05-18 19:07:33 +0300

    fix linter

--- src/components/Sum.jsx
+++ src/components/Sum.jsx
@@ -4,19 +4,11 @@ export const Sum = ({
   a,
   b,
 }) => (
-  <p>
-    Sum of
-    {a}
-    {' '}
-    and
-    {b}
-    {' '}
-    is
-    {a + b}
-  </p>
+  <p>Sum of {a} and {b} is {a + b}</p>
 );
 
 Sum.defaultProps = {
   a: 0,
   b: 0,
-};
+}
+
