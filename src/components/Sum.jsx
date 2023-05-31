aeafa8495e5a350a77b0cce74fc9338a6ae3a5fd
AlexKherson <q0992358472@gmail.com>
2023-05-31 13:30:19 +0300

    refactoring: replace function to arrow function, remove passed default params

--- src/components/Sum.jsx
+++ src/components/Sum.jsx
@@ -1,10 +1,8 @@
 /* eslint-disable react/jsx-one-expression-per-line */
 import React from 'react';
 
-export function Sum({ a = 0, b = 0 }) {
-  return (
-    <p>
-      Sum of {a} and {b} is {a + b}
-    </p>
-  );
-}
+export const Sum = ({ a = 0, b = 0 }) => (
+  <p>
+    Sum of {a} and {b} is {a + b}
+  </p>
+);
