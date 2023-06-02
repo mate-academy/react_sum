e29230b965d5851064b250fa3a4180e23d438534
Oleksii Lysenko <nano77@ukr.net>
2023-06-02 15:48:46 +0300

    Add functionality to Sum component

--- src/components/Sum.jsx
+++ src/components/Sum.jsx
@@ -1,1 +1,13 @@
-// export function Sum() {}
+import React from 'react';
+import './Sum.css';
+
+export function Sum(props) {
+  const { a = 0, b = 0 } = props;
+  const sumTxt = `Sum of ${a} and ${b} is ${a + b}`;
+
+  return (
+    <p className="sum">
+      {sumTxt}
+    </p>
+  );
+}
