4f4a87bed628859ccd17e9d07de02b9067d3151a
Daryna Hnidash <melissa998877@gmail.com>
2023-06-04 09:11:50 +0300

    fixed

--- src/components/Sum.jsx
+++ src/components/Sum.jsx
@@ -1,10 +1,5 @@
 import React from 'react';
 
-export const Sum = ({ a, b }) => {
-  <p>{`Sum of ${a} and ${b} is ${a + b}`}</p>;
-};
-
-Sum.defaultProps = {
-  a: 0,
-  b: 0,
-};
+export const Sum = ({ a = 0, b = 0 }) => (
+  <p>{`Sum of ${a} and ${b} is ${a + b}`}</p>
+);
