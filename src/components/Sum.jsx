b5a7a80c91c895ce30efe0dd736c46c203d024c9
Alla Gutsul <allagutsul1990@gmail.com>
2023-06-05 21:31:32 +0300

    improve the solution

--- src/components/Sum.jsx
+++ src/components/Sum.jsx
@@ -2,10 +2,8 @@ import React from 'react';
 
 export function Sum({ a = 0, b = 0 }) {
   return (
-    <>
-      <p>
-        {`Sum of ${a} and ${b} is ${a + b}`}
-      </p>
-    </>
+    <p>
+      {`Sum of ${a} and ${b} is ${a + b}`}
+    </p>
   );
 }
