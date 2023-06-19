bd229080e139cfb9d138fced0c7de9e35f12a297
Kateryna Stemblivska <ekaterina_09@ukr.net>
2023-06-19 19:34:38 +0300

    Correct files Readme.md and App.jsx.

--- src/App.jsx
+++ src/App.jsx
@@ -19,12 +19,5 @@ export const App = () => (
       b={5}
     />
     <Sum />
-    {/* <p>Sum of 2 and 3 is 5</p>
-    <p>Sum of -5 and 5 is 0</p>
-    <p>Sum of 10 and 0 is 10</p>
-    <p>Sum of 0 and 5 is 5</p>
-    <p>Sum of 0 and 0 is 0</p> */}
-    {/* Replace paragraphs with Sum componets */}
-    {/* And remove commented lines :) */}
   </>
 );
