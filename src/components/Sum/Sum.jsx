d63165f9589fdede966d21488a3904c304622e79
Artem Kravcov <a.kravcov1985@gmail.com>
2023-06-20 14:49:22 +0300

    React_sum

--- src/components/Sum/Sum.jsx
+++ src/components/Sum/Sum.jsx
@@ -1,1 +1,9 @@
-// export const Sum = () => ();
+import React from 'react';
+
+export function Sum({ a = 0, b = 0 }) {
+  const sum = a + b;
+
+  return (
+    <p>{`Sum of ${a} and ${b} is ${sum}`}</p>
+  );
+}
