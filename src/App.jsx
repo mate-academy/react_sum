8b55bb6ec8b01d73f3c46d8689d03e05a5589f88
Oleh Kandaskalov <olegkandaskalov@gmail.com>
2023-06-27 14:31:43 +0300

    add task solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,28 @@
 import React from 'react';
 import './App.css';
+import { Sum } from './components/Sum/Sum';
 
 export const App = () => (
   <>
-    <p>Sum of 2 and 3 is 5</p>
-    <p>Sum of -5 and 5 is 0</p>
-    <p>Sum of 10 and 0 is 10</p>
-    <p>Sum of 0 and 5 is 5</p>
-    <p>Sum of 0 and 0 is 0</p>
-    {/* Replace paragraphs with Sum componets */}
-    {/* And remove commented lines :) */}
+    <Sum
+      number1={2}
+      number2={3}
+    />
+    <Sum
+      number1={-5}
+      number2={5}
+    />
+    <Sum
+      number1={10}
+      number2={0}
+    />
+    <Sum
+      number1={0}
+      number2={5}
+    />
+    <Sum
+      number1={0}
+      number2={0}
+    />
   </>
 );
--- src/components/Sum/Sum.jsx
+++ src/components/Sum/Sum.jsx
@@ -1,1 +1,8 @@
-// export const Sum = () => ();
+export const Sum = ({
+  number1 = 0,
+  number2 = 0,
+}) => (
+  <p>
+    {`Sum of ${number1} and ${number2} is ${number1 + number2}`}
+  </p>
+);
