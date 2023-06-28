9cb8d270b651c7fe7542d8e08493bd004f509121
Daniil Postoiuk <daniil.postoyuk@gmail.com>
2023-06-28 13:19:15 +0300

    fixed code style

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,10 @@ import { Sum } from './components/Sum/Sum';
 
 export const App = () => (
   <>
-    <Sum
-      a={2}
-      b={3}
-    />
-    <Sum
-      a={-5}
-      b={5}
-    />
-    <Sum
-      a={10}
-    />
-    <Sum
-      b={5}
-    />
+    <Sum a={2} b={3} />
+    <Sum a={-5} b={5} />
+    <Sum a={10} />
+    <Sum b={5} />
     <Sum />
   </>
 );
