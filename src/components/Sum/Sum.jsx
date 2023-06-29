ef0e739b7cb678a4655ce84f8b961c7941eda28d
luka4ok <vitalik.shostak@gmail.com>
2023-06-29 16:30:32 +0300

    formating improvements

--- src/components/Sum/Sum.jsx
+++ src/components/Sum/Sum.jsx
@@ -1,11 +1,5 @@
-export const Sum = ({ a = 0, b = 0 }) => {
-  const result = a + b;
-
-  return (
-    <>
-      <p>
-        {`Sum of ${a} and ${b} is ${result}`}
-      </p>
-    </>
-  );
-};
+export const Sum = ({ a = 0, b = 0 }) => (
+  <p>
+    {`Sum of ${a} and ${b} is ${a + b}`}
+  </p>
+);
