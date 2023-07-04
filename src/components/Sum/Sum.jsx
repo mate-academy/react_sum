c955256b7f5ee1406eafb00bdb7f4d6947390522
Amadeusz Lisiecki <amadeusz.lisiecki.1993@gmail.com>
2023-07-04 12:56:21 +0200

    Add sum component

--- src/components/Sum/Sum.jsx
+++ src/components/Sum/Sum.jsx
@@ -1,1 +1,9 @@
-// export const Sum = () => ();
+export const Sum = ({ a = 0, b = 0 }) => {
+  const sumMessage = `Sum of ${a} and ${b} is ${a + b}`;
+
+  return (
+    <p>
+      {sumMessage}
+    </p>
+  );
+};
