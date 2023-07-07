94b9b8476924f0e3ecc6b54e299286b2c7307041
zipic <zpc23041988@gmail.com>
2023-07-07 18:23:34 +0300

    reactSum

--- src/components/Sum/Sum.jsx
+++ src/components/Sum/Sum.jsx
@@ -1,1 +1,9 @@
-// export const Sum = () => ();
+export const Sum = ({ a = 0, b = 0 }) => {
+  if (a === undefined && b === undefined) {
+    return <p>No params at all</p>;
+  }
+
+  return <p>{`Sum of ${a} and ${b} is ${a + b}`}</p>;
+};
+
+export default Sum;
