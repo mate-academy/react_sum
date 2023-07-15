db80a92cff99aa8da8d1ea0048623a1fb4163a3e
ArtemBehter <www.behchik@gmail.com>
2023-07-15 20:08:36 +0200

    add task solution

--- src/components/Sum/Sum.jsx
+++ src/components/Sum/Sum.jsx
@@ -1,1 +1,9 @@
-// export const Sum = () => ();
+export const Sum = ({ a = 0, b = 0 }) => {
+  const resultMessage = `Sum of ${a} and ${b} is ${a + b}`;
+
+  return (
+    <p>
+      {resultMessage}
+    </p>
+  );
+};
