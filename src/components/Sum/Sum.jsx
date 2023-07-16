593d0b152e7add4c5468efdf0691fbafbf802c46
Anna Pastushenko <annpastushenko23@gmail.com>
2023-07-16 16:18:59 +0200

    add task solution

--- src/components/Sum/Sum.jsx
+++ src/components/Sum/Sum.jsx
@@ -1,1 +1,5 @@
-// export const Sum = () => ();
+export const Sum = ({ firstNumber = 0, secondNumber = 0 }) => {
+  const sum = firstNumber + secondNumber;
+
+  return <p>{`Sum of ${firstNumber} and ${secondNumber} is ${sum}`}</p>;
+};
