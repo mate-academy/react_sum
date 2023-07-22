a1394a8cfbe71914fb45281f142b39f7e0bb39ba
Dima Astapenko <bonum_taurus@ukr.net>
2023-07-22 18:09:31 +0300

    task improved

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,8 @@ export const App = () => (
   <>
     <Sum first={2} second={3} />
     <Sum first={-5} second={5} />
-    <Sum first={10} second={0} />
-    <Sum first={0} second={5} />
+    <Sum first={10} />
+    <Sum second={5} />
     <Sum />
     {/* Replace paragraphs with Sum componets */}
     {/* And remove commented lines :) */}
