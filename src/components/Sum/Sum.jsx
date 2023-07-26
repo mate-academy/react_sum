06bd5cddb115fc2dd10ac473bc9cb98a980eca7f
Andriy Monko <Andriy17monko@gmail.com>
2023-07-26 18:55:20 +0300

    Practical task 2.0

--- src/components/Sum/Sum.jsx
+++ src/components/Sum/Sum.jsx
@@ -1,15 +1,8 @@
-import React from 'react';
-
 export const Sum = ({
   a = 0,
   b = 0,
 }) => (
-  <>
-    <p>
-      {`Sum of ${a} and ${b} is ${a + b}`}
-    </p>
-
-    {/* Replace paragraphs with Sum componets */}
-    {/* And remove commented lines :) */}
-  </>
+  <p>
+    {`Sum of ${a} and ${b} is ${a + b}`}
+  </p>
 );
