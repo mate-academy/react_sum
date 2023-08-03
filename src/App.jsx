4e2585896dc1cb4ed7f1e3e06794dc48041b5517
Hanna-Balabukha <anna.aniskovets1992@ukr.net>
2023-08-03 09:17:58 +0300

    'fixed'

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,10 @@ import { Sum } from './components/Sum/Sum';
 
 export const App = () => (
   <>
-    <p>
-      <Sum a={2} b={3} />
-    </p>
-    <p>
-      <Sum a={-5} b={5} />
-    </p>
-    <p>
-      <Sum a={10} b={0} />
-    </p>
-    <p>
-      <Sum a={0} b={5} />
-    </p>
-    <p>
-      <Sum a={0} b={0} />
-    </p>
+    <Sum a={2} b={3} />
+    <Sum a={-5} b={5} />
+    <Sum a={10} b={0} />
+    <Sum a={0} b={5} />
+    <Sum a={0} b={0} />
   </>
 );
