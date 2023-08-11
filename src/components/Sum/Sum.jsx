689bdcf87f526c4bfe3fecdac50542c443f79f2b
Anna Shevtsova <aniashevtsova@gmail.com>
2023-08-11 12:34:23 +0200

    all test pass

--- src/components/Sum/Sum.jsx
+++ src/components/Sum/Sum.jsx
@@ -1,1 +1,11 @@
-// export const Sum = () => ();
+export const Sum = ({ a = 0, b = 0 }) => {
+  const sum = a + b;
+
+  return (
+    <div className="Sum">
+      <p>
+        {`Sum of ${a} and ${b} is ${sum}`}
+      </p>
+    </div>
+  );
+};
