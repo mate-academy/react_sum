0346364dc67b6f9704e16a73495ad95059b7b415
Yuriy Bilskiy <yuriy.bilskiy@gmail.com>
2023-08-13 13:35:24 +0300

    Edits

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,21 @@ import './App.css';
 import { Sum } from './components/Sum/Sum';
 
 export const App = () => (
-    <>
-      <Sum
-        a={2}
-        b={3}
-      />
-      <Sum
-        a={-5}
-        b={5}
-      />
-      <Sum
-        a={10}
-      />
-      <Sum
-        b={5}
-      />
-      <Sum />
-    </>
+  <>
+    <Sum
+      a={2}
+      b={3}
+    />
+    <Sum
+      a={-5}
+      b={5}
+    />
+    <Sum
+      a={10}
+    />
+    <Sum
+      b={5}
+    />
+    <Sum />
+  </>
 );
