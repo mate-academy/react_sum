cf31b8e621f73d4718cdd04d28797c747bc04821
Denys14Samoilenko <d.samoilenko92@gmail.com>
2023-08-21 16:23:00 +0300

    react sum solution

--- src/components/Sum/Sum.jsx
+++ src/components/Sum/Sum.jsx
@@ -1,1 +1,11 @@
-// export const Sum = () => ();
+const DEFAULT_NUMBER_VALUE = 0;
+
+export const Sum = (
+  { a = DEFAULT_NUMBER_VALUE,
+    b = DEFAULT_NUMBER_VALUE },
+) => (
+  <p>
+    {`Sum of ${a} and ${b} is `
+      + `${a + b}`}
+  </p>
+);
