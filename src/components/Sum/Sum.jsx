932d18667989a8f22dbaf91b032116eae6a1cb66
Hubert Wiktor <hubertwiktor6@gmail.com>
2023-08-21 11:44:47 +0200

    solution

--- src/components/Sum/Sum.jsx
+++ src/components/Sum/Sum.jsx
@@ -1,1 +1,19 @@
-// export const Sum = () => ();
+export function Sum({ a = 0, b = 0 }) {
+  const sum = a + b;
+
+  return (
+    <p>
+      Sum of
+      {' '}
+      {a}
+      {' '}
+      and
+      {' '}
+      {b}
+      {' '}
+      is
+      {' '}
+      {sum}
+    </p>
+  );
+}
