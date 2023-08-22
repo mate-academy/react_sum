7d37f3f63137bca6eabe7d20b56cd3d37603f17a
Denys14Samoilenko <d.samoilenko92@gmail.com>
2023-08-22 09:40:44 +0300

    fix formatting

--- src/components/Sum/Sum.jsx
+++ src/components/Sum/Sum.jsx
@@ -1,11 +1,10 @@
 const DEFAULT_NUMBER_VALUE = 0;
 
-export const Sum = (
-  { a = DEFAULT_NUMBER_VALUE,
-    b = DEFAULT_NUMBER_VALUE },
-) => (
+export const Sum = ({
+  a = DEFAULT_NUMBER_VALUE,
+  b = DEFAULT_NUMBER_VALUE,
+}) => (
   <p>
-    {`Sum of ${a} and ${b} is `
-      + `${a + b}`}
+    {`Sum of ${a} and ${b} is ${a + b}`}
   </p>
 );
