db834cc9a3d19ea572d56829c92865357d0f86e1
andrew gidrovich <mr.androvich55555@gmail.com>
2023-08-22 15:01:38 +0300

    add task solution

--- src/components/Sum/Sum.jsx
+++ src/components/Sum/Sum.jsx
@@ -1,1 +1,9 @@
-// export const Sum = () => ();
+import React from 'react';
+
+const Sum = ({ a = 0, b = 0 }) => {
+  const sum = a + b;
+
+  return <p>{`Сума ${a} і ${b} дорівнює ${sum}`}</p>;
+};
+
+export default Sum;
