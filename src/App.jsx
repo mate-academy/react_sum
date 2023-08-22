780409648744ec16726fd7725ab3c3391e42eea2
Denys Poltorak <itwfrontend@gmail.com>
2023-08-22 10:48:26 +0300

    feat: add task solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
+import { Sum } from './components/Sum/Sum';
+
 import './App.css';
+
+const numbersToSum = [
+  { a: 2, b: 3 },
+  { a: -5, b: 5 },
+  { a: 10 },
+  { b: 5 },
+  { },
+];
 
 export const App = () => (
   <>
-    <p>Sum of 2 and 3 is 5</p>
-    <p>Sum of -5 and 5 is 0</p>
-    <p>Sum of 10 and 0 is 10</p>
-    <p>Sum of 0 and 5 is 5</p>
-    <p>Sum of 0 and 0 is 0</p>
-    {/* Replace paragraphs with Sum componets */}
-    {/* And remove commented lines :) */}
+    {numbersToSum.map(({ a, b }) => <Sum a={a} b={b} />)}
   </>
 );
