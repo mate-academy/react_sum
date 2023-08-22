33f49dda7a2995ede0d51274817a0414b862c636
Denys Poltorak <itwfrontend@gmail.com>
2023-08-22 12:39:40 +0300

    refactor: add key to rendered component

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,17 @@ import { Sum } from './components/Sum/Sum';
 import './App.css';
 
 const numbersToSum = [
-  { a: 2, b: 3 },
-  { a: -5, b: 5 },
-  { a: 10 },
-  { b: 5 },
-  { },
+  { id: 1, a: 2, b: 3 },
+  { id: 2, a: -5, b: 5 },
+  { id: 3, a: 10 },
+  { id: 4, b: 5 },
+  { id: 5 },
 ];
 
 export const App = () => (
   <>
-    {numbersToSum.map(({ a, b }) => <Sum a={a} b={b} />)}
+    {numbersToSum.map(({ a, b, id }) => (
+      <Sum key={id} a={a} b={b} />
+    ))}
   </>
 );
