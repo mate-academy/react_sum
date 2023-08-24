9a850aa2c7f830df12e85ff178f9bced8ff29a32
Oleh Obiukh <oleg.obiukh@gmail.com>
2023-08-24 23:41:00 +0100

    fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import React from "react";
-import "./App.css";
-import { Sum } from "./components/Sum/Sum";
+import React from 'react';
+import './App.css';
+import { Sum } from './components/Sum/Sum';
 
 export const App = () => (
   <>
