55c207fc1e5970498a44af3190da3f5848ac37ad
Marina Olenchenko <marina.olenchenko2204@gmail.com>
2023-09-06 19:14:01 +0300

    add readme

--- src/components/Sum/Sum.jsx
+++ src/components/Sum/Sum.jsx
@@ -1,10 +1,20 @@
 // export const Sum = () => ();
-import React from "react";
+import React from 'react';
 
 export const Sum = ({ a = 0, b = 0 }) => {
   const sum = a + b;
 
-  return <p>
-    Sum of {a} and {b} is {sum}
-    </p>;
-}
+  return (
+    <p>
+      Sum of
+      {' '}
+      {a}
+      {' '}
+      and
+      {b}
+      {' '}
+      is
+      {sum}
+    </p>
+  );
+};
