7e62f3844c3a7f9dc502c92c38be5f3857dacb4e
Marina Olenchenko <marina.olenchenko2204@gmail.com>
2023-09-11 17:02:36 +0300

    added literals

--- src/components/Sum/Sum.jsx
+++ src/components/Sum/Sum.jsx
@@ -6,15 +6,7 @@ export const Sum = ({ a = 0, b = 0 }) => {
 
   return (
     <p>
-      Sum of
-      {' '}
-      {a}
-      {' '}
-      and
-      {b}
-      {' '}
-      is
-      {sum}
+      {`Sum of ${a} and ${b} is ${sum}`}
     </p>
   );
 };
