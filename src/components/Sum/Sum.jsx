7c8590fda0595b7dabbe21009ead6a02ceefb202
Denys <denzzlle171@gmail.com>
2023-10-03 21:45:07 +0300

    addtask solution

--- src/components/Sum/Sum.jsx
+++ src/components/Sum/Sum.jsx
@@ -1,1 +1,7 @@
-// export const Sum = () => ();
+
+export const Sum = ({ firstValue = 0, secondValue = 0 }) => (
+  <p>
+    {`Sum of ${firstValue} and ${secondValue}`}
+    {` is ${firstValue + secondValue}`}
+  </p>
+);
