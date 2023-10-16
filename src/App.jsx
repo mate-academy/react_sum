f255d0cda47ff82b8a648648c5a271428cf91be9
kasiakubisiak <129735139+kasiakubisiak@users.noreply.github.com>
2023-10-16 10:16:33 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css';
 
 export const App = () => (
   <>
-    <p>Sum of 2 and 3 is 6</p>
+    <p>Sum of 2 and 3 is 5</p>
     <p>Sum of -5 and 5 is 0</p>
     <p>Sum of 10 and 0 is 10</p>
     <p>Sum of 0 and 5 is 5</p>
