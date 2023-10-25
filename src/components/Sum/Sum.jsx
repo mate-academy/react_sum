fd6d7851b18922241926e48ae20115aac23a015a
Volodymyr <psn22782@gmail.com>
2023-10-25 20:24:44 +0300

    soluton

--- src/components/Sum/Sum.jsx
+++ src/components/Sum/Sum.jsx
@@ -1,1 +1,9 @@
-// export const Sum = () => ();
+import React from 'react';
+
+export const Sum = ({ firstNum = 0, secondNum = 0 }) => (
+  <p>
+    {`Sum of ${firstNum} and ${secondNum} is ${firstNum + secondNum}`}
+  </p>
+);
+
+export default Sum;
