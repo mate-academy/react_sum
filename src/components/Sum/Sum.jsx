19d3b2d1a2184389672c8c272b0e76285e22bb26
Evhen Teneta <tenetaevgen@gmail.com>
2023-11-02 16:06:32 +0200

    task solution

--- src/components/Sum/Sum.jsx
+++ src/components/Sum/Sum.jsx
@@ -1,1 +1,10 @@
 // export const Sum = () => ();
+export function Sum({ a = 0, b = 0 }) {
+  const space = "   ";
+
+  return (
+    <p>
+      {`Sum${space}of ${a}  and  ${b}  is  ${a + b}`}
+    </p>
+  );
+}
