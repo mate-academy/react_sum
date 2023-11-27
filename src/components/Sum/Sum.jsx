8023515133ca00cbd284e6f1f77d800acdb72bb3
Patrykmclaren14 <wierzbowskipatryk5@gmail.com>
2023-11-27 22:34:09 +0100

    Solution

--- src/components/Sum/Sum.jsx
+++ src/components/Sum/Sum.jsx
@@ -1,1 +1,15 @@
-// export const Sum = () => ();
+import React from 'react';
+
+/* eslint-disable react/jsx-one-expression-per-line */
+
+export const Sum = ({ a = 0, b = 0 }) => {
+  const result = a + b;
+
+  return (
+    <p>
+      Sum of {a} and {b} is {result}
+    </p>
+  );
+};
+
+export default Sum;
