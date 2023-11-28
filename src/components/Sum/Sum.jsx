f9613f0f541c7c686aad96e2d6198e6142f4d907
Kozubowicz <55114447+Kozubowicz@users.noreply.github.com>
2023-11-28 23:08:56 +0100

    solution

--- src/components/Sum/Sum.jsx
+++ src/components/Sum/Sum.jsx
@@ -1,6 +1,6 @@
 // export const Sum = () => ();
 
-const Sum = (props) => {
+export const Sum = (props) => {
   const a = props.a !== undefined ? props.a : 0;
   const b = props.b !== undefined ? props.b : 0;
 
@@ -20,5 +20,3 @@ const Sum = (props) => {
     </p>
   );
 };
-
-export default Sum;
