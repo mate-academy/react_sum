3af69eb7a4f94db0c77c11271144726530677304
Marcin Szymanski <mszymanski1709@outlook.com>
2023-11-29 09:51:34 +0100

    fixed one tag name

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,11 @@ import './App.css';
 import { Sum } from './components/Sum/Sum';
 
 export const App = () => (
-  <>
+  <React.Fragment>
     <Sum a={2} b={3} />
     <Sum a={-5} b={5} />
     <Sum a={10} />
     <Sum b={5} />
     <Sum />
-  </>
+  </React.Fragment>
 );
