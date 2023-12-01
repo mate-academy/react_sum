2b539e5d0f9ab0936de8c4e113d017014b7c9cd5
aurelia <aurelia.gorzna.dev@gmail.com>
2023-12-01 13:06:45 +0100

    solution

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,10 @@ import { Sum } from './components/Sum/Sum';
 
 export const App = () => (
   <>
-    <p><Sum a={2} b={3} /></p>
-    <p><Sum a={-5} b={5} /></p>
-    <p><Sum a={10} /></p>
-    <p><Sum b={5} /></p>
-    <p><Sum /></p>
+    <Sum a={2} b={3} />
+    <Sum a={-5} b={5} />
+    <Sum a={10} />
+    <Sum b={5} />
+    <Sum />
   </>
 );
