c0ab0a602ca7d88d85af0214b924fcd5f437051d
aurelia <aurelia.gorzna.dev@gmail.com>
2023-12-01 12:28:59 +0100

    solution

--- src/components/Sum/Sum.jsx
+++ src/components/Sum/Sum.jsx
@@ -1,8 +1,19 @@
 /* eslint-disable react/jsx-one-expression-per-line */
 export default function Sum({ a, b }) {
+  let x = a;
+  let y = b;
+
+  if (a === undefined) {
+    x = 0;
+  }
+
+  if (b === undefined) {
+    y = 0;
+  }
+
   return (
     <p>
-      Sum of {a} and {b} is {a + b}
+      Sum of {x} and {y} is {x + y}
     </p>
   );
 }
