ddab620f9381fd07ae61cc00f8c6de397f5bf50e
Heorhii Churhuliia <gio4929189@gmail.com>
2023-12-03 12:48:13 +0000

    add task solution

--- src/components/Sum/Sum.jsx
+++ src/components/Sum/Sum.jsx
@@ -1,1 +1,5 @@
-// export const Sum = () => ();
+export const Sum = (props) => {
+  return (
+    <p>{`Sum of ${props.a ? props.a : 0} and ${props.b ? props.b : 0} is ${props.a + props.b}`}</p>
+  )
+};
