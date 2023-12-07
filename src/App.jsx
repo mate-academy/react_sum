5f619797dbe8cfe7d0b9f1f007ce6a1d73087f04
Patrykmclaren14 <wierzbowskipatryk5@gmail.com>
2023-12-07 15:37:59 +0100

    corrections

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,3 @@
-import React from 'react';
-import './App.css';
 import { Sum } from './components/Sum/Sum';
 
 export const App = () => (
--- src/components/Sum/Sum.jsx
+++ src/components/Sum/Sum.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
 
-/* eslint-disable react/jsx-one-expression-per-line */
-
 export const Sum = ({ a = 0, b = 0 }) => {
   const result = a + b;
 
   return (
     <p>
-      Sum of {a} and {b} is {result}
+      Sum of
+      {' '}
+      {a}
+      {' '}
+      and
+      {' '}
+      {b}
+      {' '}
+      is
+      {' '}
+      {result}
     </p>
   );
 };
