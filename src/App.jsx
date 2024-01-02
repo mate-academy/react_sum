50a2d8f7c198cfec3e51417084903dc0a2896c3a
Vadym Sainiuk <vadik.sajnuykk@gmail.com>
2024-01-02 16:10:39 +0200

    removed some attributes

--- src/App.jsx
+++ src/App.jsx
@@ -14,15 +14,10 @@ export const App = () => (
     />
     <Sum
       a={10}
-      b={0}
     />
     <Sum
-      a={0}
       b={5}
     />
-    <Sum
-      a={0}
-      b={0}
-    />
+    <Sum />
   </>
 );
