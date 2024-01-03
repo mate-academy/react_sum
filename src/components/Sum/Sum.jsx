c8bbff18547ee8c0c6ebde63df2f2eda947981ce
White EdenI <danil.gayvoronsky2201@gmail.com>
2024-01-04 00:47:11 +0200

    Done! (try3)

--- src/components/Sum/Sum.jsx
+++ src/components/Sum/Sum.jsx
@@ -1,5 +1,1 @@
-export const Sum = (val1 = 0, val2 = 0) => (
-  <p>
-    `Sum of ${val1} and ${val2} is ${va1 + val2}`
-  </p>
-);
+export const Sum = ({ a = 0, b = 0 }) => (<p>{`Sum of ${a} and ${b} is ${a + b}`}</p>);
