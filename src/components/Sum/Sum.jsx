77546e2b88ed29824ff63f4a64236439fe93932b
Andrii Chornyi <ixxxx@i.ua>
2024-01-12 11:19:58 +0200

    not worck

--- src/components/Sum/Sum.jsx
+++ src/components/Sum/Sum.jsx
@@ -1,1 +1,6 @@
-// export const Sum = () => ();
+export const Sum = ({ a = 0, b = 0 }) => (
+  <p>
+    {a === 0 && b === 0 ? `no params at all` : `Sum of ${a} and ${b} is ${a + b}`
+    }
+  </p>
+);
