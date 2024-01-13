9407f99f558a02112685e0d1a516bc4f271379b7
Viktoriia Melnyk <melnychka1993@gmail.com>
2024-01-13 17:05:00 +0000

    added p-s

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,9 @@ import { Sum } from './components/Sum/Sum';
 export const App = () => (
   <>
     <Sum a="2" b="3" />
-    <br />
     <Sum a="-5" b="5" />
-    <br />
-    <Sum a="10" b="0" />
-    <br />
-    <Sum a="0" b="5" />
-    <br />
-    <Sum a="0" b="0" />
+    <Sum a="10" />
+    <Sum b="5" />
+    <Sum />
   </>
 );
--- src/components/Sum/Sum.jsx
+++ src/components/Sum/Sum.jsx
@@ -1,5 +1,7 @@
 export const Sum = ({ a = 0, b = 0 }) => (
   <>
-    {`Sum of ${a} and ${b} is ${+a + +b}`}
+    <p>
+      {`Sum of ${a} and ${b} is ${+a + +b}`}
+    </p>
   </>
 );
