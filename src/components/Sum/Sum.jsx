034b70bee8c32dbfd878db396fd383c0a5079137
Serhii Prikhodko <venom99@ukr.net>
2024-01-22 15:59:39 +0200

    task is completed

--- src/components/Sum/Sum.jsx
+++ src/components/Sum/Sum.jsx
@@ -1,1 +1,16 @@
-// export const Sum = () => ();
+import React from 'react';
+
+export function Sum({ a, b }) {
+  const message = `Sum of ${a} and ${b} is ${a + b}`;
+
+  return (
+    <p>
+      {message}
+    </p>
+  );
+}
+
+Sum.defaultProps = {
+  a: 0,
+  b: 0,
+};
