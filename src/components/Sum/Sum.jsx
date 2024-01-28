af0dc758c6bdab71f2e8c821b8bb9ac79b49d63f
Siarhei Tsishkavets <kenny.sergey@gmail.com>
2024-01-28 18:13:36 +0100

    Add task solution

--- src/components/Sum/Sum.jsx
+++ src/components/Sum/Sum.jsx
@@ -1,1 +1,4 @@
-// export const Sum = () => ();
+export const Sum = ({ a = 0, b = 0 }) => (
+  // eslint-disable-next-line react/jsx-one-expression-per-line
+  <p>Sum of {a} and {b} is {a + b}</p>
+);
