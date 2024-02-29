2497b370659ef180db4a5095807df306d10f3c51
Dmytro Moshkivskyi <dmosh@ukr.net>
2024-02-29 16:44:53 +0200

    react_sum

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
 import React from 'react';
 import './App.scss';
+import { Sum } from './components/Sum/Sum';
 
 export const App = () => (
-  <>
-    <p>Sum of 2 and 3 is 5</p>
-    <p>Sum of -5 and 5 is 0</p>
-    <p>Sum of 10 and 0 is 10</p>
-    <p>Sum of 0 and 5 is 5</p>
-    <p>Sum of 0 and 0 is 0</p>
+  <div className="App">
+    <Sum
+      title1="Sum of "
+      value1={a}
+      title2=" and "
+      value2={b}
+      title3=" is "
+      value3={a + b}
+    />
+    {/* <p>Sum of 2 and 3 is 5</p> */}
+    {/* <p>Sum of -5 and 5 is 0</p> */}
+    {/* <p>Sum of 10 and 0 is 10</p> */}
+    {/* <p>Sum of 0 and 5 is 5</p> */}
+    {/* <p>Sum of 0 and 0 is 0</p> */}
     {/* Replace paragraphs with Sum componets */}
     {/* And remove commented lines :) */}
-  </>
+  </div>
 );
--- src/components/Sum/Sum.jsx
+++ src/components/Sum/Sum.jsx
@@ -1,1 +1,31 @@
-// export const Sum = () => ();
+export const Sum = () => ({
+  title1,
+  value1,
+  title2,
+  value2,
+  title3,
+  value3,
+} => (
+  <div className="Sum">
+    <h2 className="Sum__title">
+      {title1}
+    </h2>
+    <div className="Sum__value">
+      {value1}
+    </div>
+
+    <h2 className="Sum__title">
+      {title2}
+    </h2>
+    <div className="Sum__value">
+      {value2}
+    </div>
+
+    <h2 className="Sum__title">
+      {title3}
+    </h2>
+    <div className="Sum__value">
+      {value3}
+    </div>
+  </div>
+);
