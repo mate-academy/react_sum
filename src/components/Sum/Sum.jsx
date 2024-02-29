b6100950bedffa611067c54bd75a5f6a61d3d4be
Dmytro Moshkivskyi <dmosh@ukr.net>
2024-02-29 17:07:45 +0200

    react_sum by Dimoshua

--- src/components/Sum/Sum.jsx
+++ src/components/Sum/Sum.jsx
@@ -1,31 +1,5 @@
-export const Sum = () => ({
-  title1,
-  value1,
-  title2,
-  value2,
-  title3,
-  value3,
-} => (
-  <div className="Sum">
-    <h2 className="Sum__title">
-      {title1}
-    </h2>
-    <div className="Sum__value">
-      {value1}
-    </div>
-
-    <h2 className="Sum__title">
-      {title2}
-    </h2>
-    <div className="Sum__value">
-      {value2}
-    </div>
-
-    <h2 className="Sum__title">
-      {title3}
-    </h2>
-    <div className="Sum__value">
-      {value3}
-    </div>
-  </div>
+export const Sum = ({ a = 0, b = 0 }) => (
+  <p>
+    Sum of ${a} and ${b} is ${a + b}
+  </p>
 );
