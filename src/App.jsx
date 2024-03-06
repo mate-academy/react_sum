c8f0246396c61f4f95784dee503f1f2a26d1453b
Kirill <ovoo1339@gmail.com>
2024-03-06 14:36:12 +0200

    work3

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.scss';
-import { Sum } from './components/Sum';
+import { Sum } from './components/Sum.jsx';
 
 export const App = () => (
   <>
     <Sum a={2} b={3} />
-    <Sum a={-5} b={5}/>
-    <Sum a={10}/>
-    <Sum b={5}/>
+    <Sum a={-5} b={5} />
+    <Sum a={10} />
+    <Sum b={5} />
     <Sum />
   </>
 );
