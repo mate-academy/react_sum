48a4a4630f41b6890e8d61532494564ab10bcf60
Maryna Karpova <mari.artarea@gmail.com>
2024-03-18 16:42:55 +0100

    test 1

--- src/components/Sum/Sum.jsx
+++ src/components/Sum/Sum.jsx
@@ -1,1 +1,12 @@
 // export const Sum = () => ();
+const Sum = ({ a = 0, b = 0 }) => {
+  const sum = a + b;
+
+  return (
+    <p>
+      Sum of {a} and {b} is {sum}
+    </p>
+  );
+};
+
+export default { Sum };
