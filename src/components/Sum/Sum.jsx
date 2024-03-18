0f53629986372f60f393b6ee94312d3fb456659b
Vladyslav <fantom2569@gmail.com>
2024-03-18 17:32:07 +0200

    add task solution

--- src/components/Sum/Sum.jsx
+++ src/components/Sum/Sum.jsx
@@ -1,1 +1,12 @@
-// export const Sum = () => ();
+import React from 'react';
+
+export const Sum = ({ a = 0, b = 0 }) => {
+  // eslint-disable-next-line radix
+  const result = parseInt(a) + parseInt(b);
+
+  return (
+    <p>
+      Sum of {a} and {b} is {result}
+    </p>
+  );
+};
