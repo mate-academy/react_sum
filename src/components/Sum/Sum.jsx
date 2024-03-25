8b1ed4b128e8109e4b20809f2ee4564ea4ff5cc5
Wojciech Przyłuski <przyluskiwojciech@gmail.com>
2024-03-25 11:28:46 +0100

    Solution

--- src/components/Sum/Sum.jsx
+++ src/components/Sum/Sum.jsx
@@ -1,1 +1,11 @@
-// export const Sum = () => ();
+import React from 'react';
+
+export const Sum = ({ a = 0, b = 0 }) => {
+  const sum = parseInt(a, 10) + parseInt(b, 10);
+
+  return (
+    <p>
+      Sum of {a} and {b} is {sum}
+    </p>
+  );
+};
