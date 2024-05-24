dd3a29fe58a6f0f5d666f4900b8598b444e5c79d
Julia Churkina <curkinau980@gmail.com>
2024-05-24 19:50:30 +0200

    add task solution

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,9 @@ export const App = () => (
   <>
     <Sum a={2} b={3} />
     <Sum a={-5} b={5} />
-    <Sum a={10} b={Sum.b} />
-    <Sum a={5} b={Sum.b} />
-    <Sum a={Sum.a} b={Sum.b} />
+    <Sum a={10} />
+    <Sum b={5} />
+    <Sum />
   </>
 );
 
