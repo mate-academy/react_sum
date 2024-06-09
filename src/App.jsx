a4f5de6d517e8744bc58ae0910c6ec1d563f4dcd
Thiago Rocha <thiagorochakl@gmail.com>
2024-06-09 14:45:48 -0300

    Add Solution!

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import './App.scss';
+import { Sum } from './components/Sum/Sum';
 
 export const App = () => (
   <>
-    <p>Sum of 2 and 3 is 5</p>
-    <p>Sum of -5 and 5 is 0</p>
-    <p>Sum of 10 and 0 is 10</p>
-    <p>Sum of 0 and 5 is 5</p>
-    <p>Sum of 0 and 0 is 0</p>
+    <Sum numA={2} numB={3} />
+    <Sum numA={-5} numB={5} />
+    <Sum numA={10} numB={0} />
+    <Sum numA={0} numB={5} />
+    <Sum numA={0} numB={0} />
     {/* Replace paragraphs with Sum componets */}
     {/* And remove commented lines :) */}
   </>
--- src/components/Sum/Sum.jsx
+++ src/components/Sum/Sum.jsx
@@ -1,1 +1,3 @@
-// export const Sum = () => ();
+export function Sum({ numA = 0, numB = 0 }) {
+  return <p>{`Sum of ${numA} and ${numB} is ${numA + numB}`}</p>;
+}
