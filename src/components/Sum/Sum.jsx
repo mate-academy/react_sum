cdbb768c395d7a30a5943b2290bb2711304b5c43
Sa4ok <sasha.rusinka@gmail.com>
2024-06-10 13:36:43 +0300

    task

--- src/components/Sum/Sum.jsx
+++ src/components/Sum/Sum.jsx
@@ -1,1 +1,7 @@
-// export const Sum = () => ();
+export const Sum = ({ a = 0, b = 0 }) => {
+  const sum = parseInt(a) + parseInt(b);
+
+  return <p>
+    {`Sum of ${a} and ${b} is ${a + b}`}
+  </p>;
+};
