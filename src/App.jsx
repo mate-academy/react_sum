4b28b29c9735fa20ec7412f0e1a9c2dfe147fe19
Arthur Tolkachov <babanjerelo@gmail.com>
2024-06-23 14:50:50 +0300

    [Feature] added Sum component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import './App.scss';
+import { Sum } from './components/Sum/Sum';
+
+const VALUES = [
+  { id: 'value_1', a: 2, b: 3 },
+  { id: 'value_2', a: -5, b: 5 },
+  { id: 'value_3', a: 10, b: 0 },
+  { id: 'value_4', a: 0, b: 5 },
+  { id: 'value_5', a: 0, b: 0 },
+];
 
 export const App = () => (
   <>
-    <p>Sum of 2 and 3 is 5</p>
-    <p>Sum of -5 and 5 is 0</p>
-    <p>Sum of 10 and 0 is 10</p>
-    <p>Sum of 0 and 5 is 5</p>
-    <p>Sum of 0 and 0 is 0</p>
-    {/* Replace paragraphs with Sum componets */}
-    {/* And remove commented lines :) */}
+    {VALUES.map(({ a, b, id }) => (
+      <Sum key={id} a={a} b={b} />
+    ))}
   </>
 );
