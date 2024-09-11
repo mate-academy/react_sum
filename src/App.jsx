33f7539855176148c3bd8ab8684a4eb2e6938371
Wojciech Przyłuski <przyluskiwojciech@gmail.com>
2024-09-11 15:12:57 +0200

    feat: implement Sum components in App

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,9 @@ import { Sum } from './components/Sum/Sum';
 export const App = () => (
   <>
     <Sum a={2} b={3} />
-    <p>Sum of -5 and 5 is 0</p>
-    <p>Sum of 10 and 0 is 10</p>
-    <p>Sum of 0 and 5 is 5</p>
-    <p>Sum of 0 and 0 is 0</p>
-    {/* Replace paragraphs with Sum componets */}
-    {/* And remove commented lines :) */}
+    <Sum a={-5} b={5} />
+    <Sum a={10} b={0} />
+    <Sum a={0} b={5} />
+    <Sum a={0} b={0} />
   </>
 );
--- src/components/Sum/Sum.jsx
+++ src/components/Sum/Sum.jsx
@@ -1,7 +1,7 @@
 export const Sum = ({ a, b }) => {
   return (
     <p>
-      Sum of {a} and {b} is 5
+      Sum of {a} and {b} is {a + b}
     </p>
   );
 };
