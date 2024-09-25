808197a9e9aecd1b471474386abfc812fb71c811
Mykyta Forostovets <122729163+Galenwood@users.noreply.github.com>
2024-09-25 17:33:39 +0300

    react solution

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,11 @@ import './App.scss';
 import { Sum } from './components/Sum/Sum';
 
 export const App = () => (
-  <div className="App">
+  <>
     <Sum a={2} b={3} />
     <Sum a={-5} b={5} />
     <Sum a={10} />
     <Sum b={5} />
-    <Sum />
-  </div>
+    <Sum a={0} b={0} />
+  </>
 );
