14b7bb693a64ae8017126189517757bb5d78eea9
Vladimir Orlov <orelik392@gmail.com>
2024-09-27 16:26:53 +0300

    result

--- src/components/Sum/Sum.jsx
+++ src/components/Sum/Sum.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
 
 export default function Sum({ t1 = 0, t2 = 0 }) {
-  const res = t1 + t2;
-
   return (
     <p>
-      Sum of {t1} and {t2} is {res}
+      Sum of {t1} and {t2} is {t1 + t2}
     </p>
   );
 }
