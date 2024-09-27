edda88d9c0ea2c50a129d5a3a7fb0c3b3fe25396
Stepan Bahrynivskyi <bagrunivskiystepan@gmail.com>
2024-09-27 15:37:42 +0300

    task

--- src/components/Sum/Sum.jsx
+++ src/components/Sum/Sum.jsx
@@ -1,1 +1,17 @@
-// export const Sum = () => ();
+export const Sum = ({ a = 0, b = 0 }) => (
+  <p>
+    Sum of {a} and {b} is {a + b}.
+  </p>
+);
+
+export const Sub = ({ a = 0, b = 0 }) => (
+  <p>
+    Diference of {a} and {b} is {a - b}.
+  </p>
+);
+
+export const Mul = ({ a = 0, b = 0 }) => (
+  <p>
+    Mul of {a} and {b} is {a * b}.
+  </p>
+);
