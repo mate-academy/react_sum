a8a2a3d39218b6e25cdea3360c12c8e674716a08
Yulian47 <78698459+Yulian47@users.noreply.github.com>
2025-03-04 13:57:21 +0100

    add task solution

--- src/components/Sum/Sum.jsx
+++ src/components/Sum/Sum.jsx
@@ -1,1 +1,20 @@
-// export const Sum = () => ();
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export const Sum = ({ a, b }) => {
+  return (
+    <p>
+      Sum of {a} and {b} is {a + b}
+    </p>
+  );
+};
+
+Sum.propTypes = {
+  a: PropTypes.number,
+  b: PropTypes.number,
+};
+
+Sum.defaultProps = {
+  a: 0,
+  b: 0,
+};
