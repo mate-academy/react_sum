34ef09970d982f629278b3b4c88d0d9e80593253
Andrew Siemakin <andrewsiemakin@gmail.com>
2025-04-06 02:22:13 +0300

    Fixed

--- src/components/Sum/Sum.jsx
+++ src/components/Sum/Sum.jsx
@@ -1,9 +1,5 @@
 import React from 'react';
 
 export const Sum = ({ a = 0, b = 0 }) => {
-  return (
-    <p>
-      Sum of {a} and {b} is {a + b}
-    </p>
-  );
+  return <p>{`Sum of ${a} and ${b} is ${a + b}`}</p>;
 };
