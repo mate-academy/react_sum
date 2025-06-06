c86ec5642a4c8f9556e9b52c3099986f3c4f967e
Your Name <lipe291@gmail.com>
2025-06-06 08:30:56 -0300

    add task solution

--- src/components/Sum/Sum.jsx
+++ src/components/Sum/Sum.jsx
@@ -1,1 +1,11 @@
-// export const Sum = () => ();
+export const Sum = ({ a = 0, b = 0 }) => {
+  const soma = a + b;
+
+  return (
+    <div>
+      <p>
+        Sum of {a} and {b} is {soma}
+      </p>
+    </div>
+  );
+};
