766d1d0c59e55368be16067e89f6e2e01f8b8be5
Liudmyla Panchenko <liudmyla.panchenko29@gmail.com>
2025-06-26 18:23:42 +0300

    react sum task solution

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,8 @@ export const App = () => (
   <>
     <Sum a={2} b={3} />
     <Sum a={-5} b={5} />
-    <Sum a={10} b={0} />
-    <Sum a={0} b={5} />
-    <Sum a={0} b={0} />
-    <p>Sum of 0 and 5 is 5</p>
-    <p>Sum of 0 and 0 is 0</p>
+    <Sum a={10} />
+    <Sum b={5} />
+    <Sum />
   </>
 );
