9ad55e4d2be174988e1e6c12e0321e67720cfd33
Emaan Hookey <em.grey28@gmail.com>
2025-08-02 13:18:14 -0230

    followed insructions

--- src/components/Sum/Sum.jsx
+++ src/components/Sum/Sum.jsx
@@ -1,1 +1,9 @@
-// export const Sum = () => ();
+export const Sum = ({ a = 0, b = 0 }) => {
+  const result = a + b;
+
+  return (
+    <p>
+      The sum of {a} and {b} is {result}.
+    </p>
+  );
+};
