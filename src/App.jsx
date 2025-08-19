33f45628f74cb53bc199d72f6aa760e8dd4fa05d
Denis <denchick1546450@gmail.com>
2025-08-20 01:43:31 +0300

    add solution

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,5 @@ export const App = () => (
     <Sum a={10} />
     <Sum b={5} />
     <Sum />
-    {/* <p>Sum of -5 and 5 is 0</p>
-    <p>Sum of 10 and 0 is 10</p>
-    <p>Sum of 0 and 5 is 5</p>
-    <p>Sum of 0 and 0 is 0</p> */}
-    {/* Replace paragraphs with Sum componets */}
-    {/* And remove commented lines :) */}
   </>
 );
--- src/components/Sum/Sum.jsx
+++ src/components/Sum/Sum.jsx
@@ -1,5 +1,5 @@
 export const Sum = ({ a = 0, b = 0 }) => (
   <p>
-    Sum of {a} and {b} is {a + b}
+    {`Sum of ${a} and ${b} is ${a + b}`}
   </p>
 );
