0b6d5a72f7aa3c7d07ea996a84760f081b855b5a
FRANCISCOPELEGRINI <franciscoprcunha@gmail.com>
2025-09-02 16:04:30 -0300

    100%

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
-import React from 'react';
+
 import './App.scss';
+import { Sum } from './components/Sum';
 
-
-export const App = ({ a = 0, b = 0 }) => {
-  return <p>Sum of {a} and {b} is {a + b}</p>;
-};
+export const App = () => (
+  
+  <>
+    <Sum a={1} b={2} />
+    <Sum a={3} b={4} />
+    <Sum a={5} b={6} />
+    <Sum a={7} b={8} />
+    <Sum /> 
+  </>
+);
 
