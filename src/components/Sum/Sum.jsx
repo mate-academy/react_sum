4509ac6328683c49e20e0738401286395b12761e
FRANCISCOPELEGRINI <franciscoprcunha@gmail.com>
2025-09-02 16:16:48 -0300

    AGORA SIM PAPI

--- src/components/Sum/Sum.jsx
+++ src/components/Sum/Sum.jsx
@@ -1,4 +1,6 @@
-
+import React from 'react';
 export const Sum = ({ a = 0, b = 0 }) => {
-    return <p>Sum of {a} and {b} is {a + b}</p>;
-}
+  return (
+    <p>Sum of {a} and {b} is {a + b}</p>
+  );
+};
