0ccd56893b8de51130fe59c77f91ce5d88603404
FRANCISCOPELEGRINI <franciscoprcunha@gmail.com>
2025-09-02 15:53:32 -0300

    AGORA VAI

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,8 @@
 import React from 'react';
 import './App.scss';
-import { Sum } from './Sum';
 
-export const App = () => (
-  <>
-    <Sum a={2} b={3} />
-    <Sum a={-5} b={5} />
-    <Sum a={10} />
-    <Sum b={5} />
-    <Sum />
-  </>
-);
+
+export const App = ({ a = 0, b = 0 }) => {
+  return <p>Sum of {a} and {b} is {a + b}</p>;
+};
+
