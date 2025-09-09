944daee0f41aa503c75111185cbf8c032d9f7c6d
z3rt <z3rt@MacBook-Pro-Aleksandr.local>
2025-09-09 21:51:19 +0200

    solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
+/* eslint-disable import/extensions */
 import React from 'react';
 import './App.scss';
-import { Sum } from './components/Sum/Sum';
+import { Sum } from './components/Sum/Sum.jsx';
 
 export const App = () => (
   <>
