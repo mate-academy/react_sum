20f090e72ead304dab7a4bf5c16e1c80f7995cb2
Anton <nimachuka@gmail.com>
2025-10-19 17:40:20 +0200

    all test has been passed

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.scss';
-import{Sunm} from './components/Sum/Sum.jsx';
+import { Sum } from './components/Sum/Sum';
 
 export const App = () => (
   <>
     <Sum a={2} b={3} />
     <Sum a={-5} b={5} />
-    <Sum a={10} b={0} />
-    <Sum a={0} b={5} />
-    <Sum a={-3} b={-7} />
+    <Sum a={10} />
+    <Sum b={5} />
+    <Sum />
   </>
 );
--- src/components/Sum/Sum.jsx
+++ src/components/Sum/Sum.jsx
@@ -1,3 +1,13 @@
-export const Sum = (a, b) => {
-  return <p>Sum of {a} and {b} is {a + b}</p>;
+import React from 'react';
+
+export const Sum = ({ a = 0, b = 0 }) => {
+  const aMissing = a === undefined;
+  const bMissing = b === undefined;
+
+  const aText = aMissing ? 'nothing' : a;
+  const bText = bMissing ? 'nothing' : b;
+
+  const result = (aMissing ? 0 : a) + (bMissing ? 0 : b);
+
+  return <p>{`Sum of ${aText} and ${bText} is ${result}`}</p>;
 };
