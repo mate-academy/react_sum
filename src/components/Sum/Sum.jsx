42bbd4a0817377fd37f283cedf1f06d219bed646
Anton <nimachuka@gmail.com>
2025-10-22 19:22:19 +0200

    mistakes was fixed

--- src/components/Sum/Sum.jsx
+++ src/components/Sum/Sum.jsx
@@ -1,13 +1,7 @@
 import React from 'react';
 
 export const Sum = ({ a = 0, b = 0 }) => {
-  const aMissing = a === undefined;
-  const bMissing = b === undefined;
+  const result = a + b;
 
-  const aText = aMissing ? 'nothing' : a;
-  const bText = bMissing ? 'nothing' : b;
-
-  const result = (aMissing ? 0 : a) + (bMissing ? 0 : b);
-
-  return <p>{`Sum of ${aText} and ${bText} is ${result}`}</p>;
+  return <p>{`Sum of ${a} and ${b} is ${result}`}</p>;
 };
