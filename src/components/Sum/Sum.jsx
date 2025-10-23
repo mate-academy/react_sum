dd52508b4faa155418f96510e17778fa9c2899c8
Nadiia Shishova <nadin71983@gmail.com>
2025-10-23 23:20:59 +0300

    add task solution

--- src/components/Sum/Sum.jsx
+++ src/components/Sum/Sum.jsx
@@ -3,9 +3,5 @@
 import React from 'react';
 
 export const Sum = ({ a = 0, b = 0 }) => {
-  return (
-    <p>
-      Sum of {a} and {b} is {a + b}
-    </p>
-  );
+  return <p>{`Sum of ${a} and ${b} is ${a + b}`}</p>;
 };
