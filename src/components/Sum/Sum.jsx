181aab1740049842343817f7bebc856b62669804
Roman Mazuryk <romazuryk@gmail.com>
2025-11-05 14:40:23 +0100

    Refactor Sum component to use template literal in JSX

--- src/components/Sum/Sum.jsx
+++ src/components/Sum/Sum.jsx
@@ -1,10 +1,3 @@
-// export const Sum = () => ();
 export function Sum({ a = 0, b = 0 }) {
-    const result = a + b;
-
-    return (
-        <p>
-            Sum of {a} and {b} is {result}
-        </p>
-    );
+  return <p>{`Sum of ${a} and ${b} is ${a + b}`}</p>;
 }
