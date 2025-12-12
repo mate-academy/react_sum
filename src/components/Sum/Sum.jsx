e09af0808a7ee32b2b642e7fbfcbc5b8ad4a187c
mpodaniev <mpodanev@gmail.com>
2025-12-12 14:50:36 +0200

    Simplify the ` Sum ` component by removing the unnecessary ` < div > ` wrapper.

--- src/components/Sum/Sum.jsx
+++ src/components/Sum/Sum.jsx
@@ -1,9 +1,3 @@
 export const Sum = ({ a = 0, b = 0 }) => {
-  return (
-    <div>
-      <p>
-        Sum of {a} and {b} is {a + b}
-      </p>
-    </div>
-  );
+  return <p>{`Sum of ${a} and ${b} is ${a + b}`}</p>;
 };
