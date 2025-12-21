77c3a8a63cf0a43acf26c0be22fceca87cb1f037
Natalia Yakovleva <yakovlevane29@gmail.com>
2025-12-21 12:31:05 +0200

    fixed solution

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,9 @@ import { Sum } from './components/Sum/Sum';
 
 export const App = () => (
   <>
-    <Sum a={7} b={10} />
-    <Sum a={6} b={-4} />
-    <Sum a={-40} b={-2} />
+    <Sum a={2} b={3} />
+    <Sum a={-5} b={5} />
+    <Sum a={10} b={0} />
     <Sum a={0} b={5} />
     <Sum a={0} b={0} />
     {/* Replace paragraphs with Sum componets */}
