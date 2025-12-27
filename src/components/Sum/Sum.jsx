147395ce71132b1a65d16b647cab44a5972ccfbe
Alex <1inboxna@gmail.com>
2025-12-27 23:07:34 +0200

    commit message-2

--- src/components/Sum/Sum.jsx
+++ src/components/Sum/Sum.jsx
@@ -1,11 +1,5 @@
 // export const Sum = () => ();
 
 export function Sum({ a = 0, b = 0 }) {
-  return (
-    <p>
-      Sum of {a} and {b} is {a + b}
-    </p>
-  );
+  return <p>{`Sum of ${a} and ${b} is ${a + b}`}</p>;
 }
-
-export default Sum;
