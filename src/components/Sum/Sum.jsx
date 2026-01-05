c99af4f994e4cdded9660c8d7a1c5df50b2ac454
Vasyl Levitskyi <leva1990vale@gmail.com>
2026-01-05 11:29:06 +0200

    add solution

--- src/components/Sum/Sum.jsx
+++ src/components/Sum/Sum.jsx
@@ -1,1 +1,10 @@
 // export const Sum = () => ();
+import React from 'react';
+
+export const Sum = ({ a = 0, b = 0 }) => {
+  return (
+    <p>
+      Sum of {a} and {b} is {a + b}
+    </p>
+  );
+};
