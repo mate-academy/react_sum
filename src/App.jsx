c8bff9bdc0925885f8010aa0b8b7404e87f18c7e
Aleks Pashkevych <alekspashkevych@gmail.com>
2026-03-01 17:15:43 +0200

    task done, looks working, but tests not passing

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import './App.scss';
+import { Sum } from './components/Sum/Sum';
 
 export const App = () => (
   <>
-    <p>Sum of 2 and 3 is 5</p>
-    <p>Sum of -5 and 5 is 0</p>
-    <p>Sum of 10 and 0 is 10</p>
-    <p>Sum of 0 and 5 is 5</p>
-    <p>Sum of 0 and 0 is 0</p>
-    {/* Replace paragraphs with Sum componets */}
-    {/* And remove commented lines :) */}
+    <Sum val1={2} val2={3} />
+    <Sum val1={-5} val2={5} />
+    <Sum val1={10} val2={0} />
+    <Sum val1={0} val2={5} />
+    <Sum val1={0} val2={0} />
   </>
 );
--- src/components/Sum/Sum.jsx
+++ src/components/Sum/Sum.jsx
@@ -1,1 +1,6 @@
 // export const Sum = () => ();
+export const Sum = ({ val1 = 0, val2 = 0 }) => (
+  <p>
+    Sum of {val1} and {val2} is {val1 + val2}
+  </p>
+);
