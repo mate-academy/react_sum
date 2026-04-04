3b651deaf5b4882b463fa6e2cf8007d3d0636211
yevhen.ivchenko <yevhen.ivchenko@cleverbridge.com>
2026-04-04 18:48:22 +0300

    Add Sum component

--- src/components/Sum/Sum.jsx
+++ src/components/Sum/Sum.jsx
@@ -1,1 +1,9 @@
-// export const Sum = () => ();
+import './Sum.scss';
+
+export const Sum = ({ a = 0, b = 0 }) => {
+  return (
+    <div className="Sum">
+      <p className="Sum__text">{`Sum of ${a} and ${b} is ${a + b}`}</p>
+    </div>
+  );
+};
