207eaac06af5984c49c66b7638f39479592f2b64
Roman Lysunets <rlysunets@gmail.com>
2026-04-21 16:54:11 +0300

    add task solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import './App.scss';
+import { Sum } from './components/Sum/Sum';
 
 export const App = () => (
   <>
-    <p>Sum of 2 and 3 is 5</p>
-    <p>Sum of -5 and 5 is 0</p>
-    <p>Sum of 10 and 0 is 10</p>
-    <p>Sum of 0 and 5 is 5</p>
-    <p>Sum of 0 and 0 is 0</p>
+    <Sum a={2} b={3} />
+    <Sum a={-5} b={5} />
+    <Sum a={10} b={0} />
+    <Sum a={0} b={5} />
+    <Sum a={0} b={0} />
     {/* Replace paragraphs with Sum componets */}
     {/* And remove commented lines :) */}
   </>
