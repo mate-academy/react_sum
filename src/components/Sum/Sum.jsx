af2d3a01f14b3709eec2d99c7535070591529aac
linska <linskatanya@gmail.com>
2026-04-22 13:47:09 +0300

    feat: add sum component

--- src/components/Sum/Sum.jsx
+++ src/components/Sum/Sum.jsx
@@ -1,7 +1,3 @@
 export const Sum = ({ a = 0, b = 0 }) => {
-  return (
-    <p>
-      <p>{`Sum of ${a} and ${b} is ${a + b}`}</p>
-    </p>
-  );
+  return <p>{`Sum of ${a} and ${b} is ${a + b}`}</p>;
 };
