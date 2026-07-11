11fe56d9fefc3caecf434ab6a7c62a875a811b4c
student1232354 <tarasromanenko33@gmail.com>
2026-07-11 12:57:28 +0300

    second update

--- src/components/Sum/Sum.jsx
+++ src/components/Sum/Sum.jsx
@@ -1,5 +1,5 @@
-export const Sum = ({ a = 0, b = 0, c = a + b }) => (
+export const Sum = ({ a = 0, b = 0 }) => (
   <p>
-    Sum of {a} and {b} is {c}
+    Sum of {a} and {b} is {a + b}
   </p>
 );
