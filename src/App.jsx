627ce8e7895d4f0300139170f9b1d9c4ae4cb38a
Olga Shcherbatiuk <nika32.n3@gmail.com>
2026-08-24 21:09:53 +0300

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
 import React from 'react';
 import './App.scss';
-
-export const Sum = ({ a = 0, b = 0 }) => (
-  <p>{`Sum of ${a} and ${b} is ${a + b}`}</p>
-);
+import { Sum } from './components/Sum/Sum';
 
 export const App = () => (
   <>
     <Sum a={2} b={3} />
     <Sum a={-5} b={5} />
-    <Sum a={10} b={0} />
-    <Sum a={0} b={5} />
-    <Sum a={0} b={0} />
+    <Sum a={10} />
+    <Sum b={5} />
+    <Sum />
   </>
 );
