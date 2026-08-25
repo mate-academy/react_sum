c0f0561346c8bbe216ce791acac94ea52c4e18af
Artem Mayboroda <artmay77777@gmail.com>
2026-08-26 00:48:32 +0300

    Create Sum component

--- src/components/Sum/Sum.jsx
+++ src/components/Sum/Sum.jsx
@@ -1,1 +1,7 @@
-// export const Sum = () => ();
+export const Sum = ({ a = 0, b = 0 }) => {
+  const calculateSum = (x, y) => {
+    return x + y;
+  };
+
+  return <p>{`Sum of ${a} and ${b} is ${calculateSum(a, b)}`}</p>;
+};
