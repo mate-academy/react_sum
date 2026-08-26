d381b496eb67a707452affdc779c158f74fca16f
Anton Radchenko <radchenkotoshka@gmail.com>
2026-08-27 00:41:45 +0300

    solved problems from the review

--- src/components/Sum/Sum.jsx
+++ src/components/Sum/Sum.jsx
@@ -1,11 +1,3 @@
 export const Sum = ({ a = 0, b = 0 }) => {
-  function calculateSum(x, y) {
-    return x + y;
-  }
-
-  return (
-    <p>
-      Sum of {a} and {b} is {calculateSum(a, b)}
-    </p>
-  );
-}
+  return (<p>{`Sum of ${a} and ${b} is ${a + b}`}</p>);
+};
