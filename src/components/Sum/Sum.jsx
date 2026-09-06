c232c263519f4d2462386acd4e4c3962bd601e6a
Roman Matej <matej.roman@webcom.cz>
2026-09-06 19:50:20 +0200

    Fix calculation logic in Sum component to ensure correct sum of a and b

--- src/components/Sum/Sum.jsx
+++ src/components/Sum/Sum.jsx
@@ -1,6 +1,6 @@
 // export const Sum = () => ();
 export const Sum = ({a,b}) =>{
-    const result = a || 0 + b || 0;
+    const result = (a || 0) + (b || 0);
     return (
         <p>
             `Sum of ${a} and {b} is {result}`
